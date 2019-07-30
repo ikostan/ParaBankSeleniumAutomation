2476f3ff6025afc22454700589c2d6872e85f28b
Egor Kostan <igorkostan@gmail.com>
2019-07-30 13:57:06 -0700

    Update base_page_model.py

--- page_models/base_page_model.py
+++ page_models/base_page_model.py
@@ -3,8 +3,17 @@ import selenium.webdriver
 
 class BasePageModel:
 
-	def __init__(self, driver: selenium.webdriver):
+	def __init__(self, driver: selenium.webdriver, implicit_wait_time: 0):
+
+		if type(implicit_wait_time) != int:
+			raise TypeError('ERROR: wrong data type. Please set "implicit_wait_time" value as integer.')
+
 		self._driver = self._set_driver(driver)
+		
+		# The default value of time that can be set using Implicit wait is zero.
+		# Its unit is in seconds.
+		# Implicit wait remains associated with the web element until it gets destroyed.
+		self._driver.implicitly_wait(implicit_wait_time)
 
 	@property
 	def driver(self):
