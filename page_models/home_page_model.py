2bbc8c7f9de50b53f471d26fe069d30cffb42d13
Egor Kostan <igorkostan@gmail.com>
2019-07-30 15:45:47 -0700

    Create home_page_model.py

---
+++ page_models/home_page_model.py
@@ -0,0 +1,16 @@
+import selenium.webdriver
+from utils.driver import Driver
+from selenium.webdriver.common.by import By
+from page_context.home_page_context import HomePageContext
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, InvalidElementStateException
+
+
+class HomePageModel:
+	'''
+	The page object pattern intends creating an object for each web page.
+	By following this technique a layer of separation between the test code and technical implementation is created.
+	'''
+
+	_url = HomePageContext.URL
