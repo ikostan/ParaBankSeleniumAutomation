62ea412b8a63e7097e41e47970ef7336e83146cc
Egor Kostan <igorkostan@gmail.com>
2019-07-29 22:22:40 -0700

    Create path_config.py

---
+++ utils/path_config.py
@@ -0,0 +1,20 @@
+class DriverPath:
+
+    #CHROME_WEB_DRIVER_PATH = '\\drivers\\chrome\\win32\\74.0.3729.6\\chromedriver.exe'
+    CHROME_WEB_DRIVER_PATH = '\\drivers\\chrome\\win32\\75.0.3770.140\\chromedriver.exe'
+
+    EDGE_WEB_DRIVER_PATH = '\\drivers\\microsoft_edge\\release_17134\\MicrosoftWebDriver.exe'
+
+    MOZILLA_WEB_DRIVER_PATH = '\\drivers\\mozilla_geckodriver\\v0.24.0\\win64\\geckodriver.exe'
+
+    IE_WEB_DRIVER_PATH = '\\drivers\\IE_Driver_Server\\x86\\3.14.0\\IEDriverServer.exe'
+
+    OPERA_WEB_DRIVER_PATH = '\\drivers\\opera\\win_64\\2.45\\operadriver.exe'
+    #OPERA_WEB_DRIVER_PATH = '\\drivers\\opera\\win_64\\75.0.3770.100\\operadriver.exe'
+
+    OPERA_BINARY_PATH = r'C:\Users\superadmin\AppData\Local\Programs\Opera\60.0.3255.95\opera.exe'
+    #OPERA_BINARY_PATH = 'C:\\Users\\superadmin\\AppData\\Local\\Programs\\Opera\\62.0.3331.99\\opera.exe'
+
+
+
+
