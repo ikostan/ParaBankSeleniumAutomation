cecb2533021abb9e206e6b3dee6c83359a0d44d8
Egor Kostan <igorkostan@gmail.com>
2019-07-30 14:28:11 -0700

    Update home_page_context.py

--- page_context/home_page_context.py
+++ page_context/home_page_context.py
@@ -14,6 +14,15 @@ class HomePageContext(BasePageContext):
 		'title': 'ATM Services',
 		'Withdraw Funds': {'text': 'Withdraw Funds',
 		                   'href': "services/ParaBank?wsdl"},
+		'Transfer Funds': {'text': 'Transfer Funds',
+		                   'href': "services/ParaBank?wsdl"},
+		'Check Balances': {'text': 'Check Balances',
+		                   'href': "services/ParaBank?wsdl"},
+		'Make Deposits': {'text': 'Make Deposits',
+		                  'href': "services/ParaBank?wsdl"}
+	}
 
+	ONLINE_SERVICES = {
+		
 	}
 
