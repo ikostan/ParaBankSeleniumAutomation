a069a4af2c587f8988499072fd4bf2442b24fb99
Egor Kostan <igorkostan@gmail.com>
2019-07-30 11:56:27 -0700

    Update base_page_context.py

--- page_context/base_page_context.py
+++ page_context/base_page_context.py
@@ -1,2 +1,25 @@
 class BasePageContext:
-	pass
+	'''
+	Holds expected context values for relevant page items
+	'''
+
+	ROOT_URL = 'https://parabank.parasoft.com/parabank'
+	URL = ROOT_URL + '/index.htm'
+
+	TITLE = 'ParaBank | Welcome | Online Banking'
+	SLOGAN = 'Experience the difference'
+
+	LEFT_MENU_ITEMS = {
+		'Solutions': None,
+		'About Us': 'about.htm',
+		'Services': 'services.htm',
+		'Products': 'http://www.parasoft.com/jsp/products.jsp',
+		'Locations': 'http://www.parasoft.com/jsp/pr/contacts.jsp',
+		'Admin Page': 'admin.htm'
+	}
+
+	MENU_BUTTONS = {
+		'home': "/parabank/index.htm",
+		'about': "about.htm",
+		'contact': "contact.htm"
+	}
