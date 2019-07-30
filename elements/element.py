e49803124abe92016c21ba17fc4bb72a0ae8edb8
Egor Kostan <igorkostan@gmail.com>
2019-07-30 11:33:11 -0700

    Update element.py

--- elements/element.py
+++ elements/element.py
@@ -1,13 +1,18 @@
 from elements.base_element import BaseElement
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, InvalidElementStateException
 
 
 class Element(BaseElement):
 
 	@property
 	def text(self):
+		'''
+		Returns inner text or NoSuchElementException
+		in case element is invisible or not present on the DOM
+		:return:
+		'''
 		try:
 			element = WebDriverWait(super().driver, 10).until(EC.visibility_of_element_located(super().locator))
 			text = element.text
@@ -17,10 +22,15 @@ class Element(BaseElement):
 			raise NoSuchElementException('ERROR: can not find element. The element is not present on the DOM or invisible.')
 
 	def click(self):
+		'''
+		Clicks on web element or returns InvalidElementStateException
+		in case the element is invisible or not clickable.
+		:return:
+		'''
 		try:
 			element = WebDriverWait(super().driver, 10).until(EC.element_to_be_clickable(super().locator))
 			text = element.click()
 			return None
 
 		except TimeoutException as ec:
-			raise NoSuchElementException('ERROR: can not find element. The element is invisible or not clickable.')
+			raise InvalidElementStateException('ERROR: can not find element. The element is invisible or not clickable.')
