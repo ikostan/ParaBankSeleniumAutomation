349de4faf7e45ba8cd7d4877512658901d064b84
Egor Kostan <igorkostan@gmail.com>
2019-07-30 14:58:36 -0700

    Update about_page_context.py

--- page_context/about_page_context.py
+++ page_context/about_page_context.py
@@ -2,4 +2,13 @@ from page_context.base_page_context import BasePageContext
 
 
 class AboutPageContext(BasePageContext):
-	pass
+
+	TITLE = BasePageContext.TITLE + 'About Us'
+	URL = BasePageContext.ROOT_URL + 'about.htm'
+
+	DESCRIPTION = {'title': 'ParaSoft Demo Website',
+	               'text': ['ParaBank is a demo site used for demonstration of Parasoft software solutions.',
+	                        'All materials herein are used solely for simulating a realistic online banking website.',
+	                        'In other words: ParaBank is not a real bank!',
+	                        'For more information about Parasoft solutions please visit us at:\nwww.parasoft.com or '
+	                        'call 888-305-0041']}
