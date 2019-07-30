f514189a50b99693f8b4c6c0c0dd381bcae07d4b
Egor Kostan <igorkostan@gmail.com>
2019-07-30 14:00:45 -0700

    Update base_page_locator.py

--- page_locators/base_page_locator.py
+++ page_locators/base_page_locator.py
@@ -2,4 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocator:
+	'''
+	Holds all relevant locators for page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
 	pass
