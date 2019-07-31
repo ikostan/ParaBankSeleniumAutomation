2bb45b6a94d22f5c3fe7b4abc3bdd02126f75e1b
Egor Kostan <igorkostan@gmail.com>
2019-07-30 19:38:37 -0700

    Create about_page_locator.py

---
+++ page_locators/about_page_locator.py
@@ -0,0 +1,10 @@
+from page_locators.base_page_locator import BasePageLocator
+
+
+class AboutPageLocator(BasePageLocator):
+	'''
+	Holds all relevant locators for 'ABOUT' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
+	pass
