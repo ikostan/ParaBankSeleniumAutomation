f10e6ab613ded15fe5520da6d46a2831f43db15e
Egor Kostan <igorkostan@gmail.com>
2019-07-30 21:19:00 -0700

    Update base_element.py

--- elements/base_element.py
+++ elements/base_element.py
@@ -1,4 +1,10 @@
 import selenium.webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, \
+	NoSuchElementException, \
+	InvalidElementStateException, \
+	NoSuchAttributeException
 
 
 class BaseElement:
@@ -6,6 +12,7 @@ class BaseElement:
 	def __init__(self, driver: selenium.webdriver, locator=None):
 		self._driver = self._set_driver(driver)
 		self._locator = self._set_locator(locator)
+		self._element = self._find_element()
 
 	@property
 	def driver(self):
@@ -15,6 +22,24 @@ class BaseElement:
 		'''
 		return self._driver
 
+	def attribute(self, attribute: str):
+		'''
+		Returns attribute value
+		:return:
+		'''
+		try:
+			atr = self.element.get_attribute(attribute)
+			return atr
+
+		except TimeoutException:
+			raise NoSuchAttributeException(
+				'\nERROR: The element has no attribute "{}".\n'
+				'LOCATOR: {}\n'.format(attribute, super().locator))
+
+	@property
+	def element(self):
+		return self._element
+
 	@property
 	def locator(self):
 		'''
@@ -22,6 +47,20 @@ class BaseElement:
 		:return:
 		'''
 		return self._locator
+
+	def _find_element(self):
+		'''
+		Returns web element or NoSuchElementException in case element does not exist on the DOM
+		:return:
+		'''
+		try:
+			element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.locator))
+			return element
+
+		except TimeoutException:
+			raise NoSuchElementException(
+				'\nERROR: can not find element. The element is not present on the DOM.\n'
+				'LOCATOR: {}\n'.format(self.locator))
 
 	@staticmethod
 	def _set_driver(driver: selenium.webdriver):
