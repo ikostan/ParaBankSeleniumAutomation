3818ffddba36047b03bf482758c0f0f4752db7f3
Egor Kostan <igorkostan@gmail.com>
2019-07-30 19:38:06 -0700

    Update home_page_test.py

--- tests/home_page_test.py
+++ tests/home_page_test.py
@@ -6,10 +6,17 @@ from page_context.home_page_context import HomePageContext
 
 class MyTestCase(BaseTestCase):
 
-	def test_home_page_is_up(self):
+	def test_context_base_elements_chrome(self):
+
+		# Open web page
 		browser = 'chrome'
 		self.driver = Driver(browser)
 		self.page = HomePageModel(self.driver)
 		self.page.go()
+
+		# Test base context:
+		self.assertEqual(HomePageContext.URL, self.page.url)
+		self.assertEqual(HomePageContext.SLOGAN, self.page.slogan)
 		self.assertEqual(HomePageContext.TITLE, self.page.title)
+		self.assertEqual(HomePageContext.ADMIN_LOGO['href'], self.page.admin_logo_href)
 
