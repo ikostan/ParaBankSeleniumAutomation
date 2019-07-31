4ffaa2fc79966308fbce3669f1646ef8aee6e82c
Egor Kostan <igorkostan@gmail.com>
2019-07-30 23:21:59 -0700

    Update home_page_test.py

--- tests/home_page_test.py
+++ tests/home_page_test.py
@@ -15,20 +15,43 @@ class HomePageTestCase(BaseTestCase):
 		self.page.go()
 
 		# Test base context:
-		self.assertEqual(HomePageContext.URL, self.page.url)
-		self.assertEqual(HomePageContext.SLOGAN, self.page.slogan)
-		self.assertEqual(HomePageContext.TITLE, self.page.title)
+		self.assertEqual(HomePageContext.URL,
+		                 self.page.url)
+		self.assertEqual(HomePageContext.SLOGAN,
+		                 self.page.slogan)
+		self.assertEqual(HomePageContext.TITLE,
+		                 self.page.title)
 
-		self.assertEqual(HomePageContext.ADMIN_LOGO['href'], self.page.admin_logo_formated_href)
-		self.assertEqual(HomePageContext.ADMIN_LOGO['class'], self.page.admin_logo_img_class)
-		self.assertEqual(HomePageContext.ADMIN_LOGO['src'], self.page.admin_logo_formated_img_src)
+		self.assertEqual(HomePageContext.ADMIN_LOGO['href'],
+		                 self.page.admin_logo_formated_href)
+		self.assertEqual(HomePageContext.ADMIN_LOGO['class'],
+		                 self.page.admin_logo_img_class)
+		self.assertEqual(HomePageContext.ADMIN_LOGO['src'],
+		                 self.page.admin_logo_formated_img_src)
 
-		self.assertEqual(HomePageContext.PARA_BANK_LOGO['href'], self.page.para_bank_logo_formated_href)
-		self.assertEqual(HomePageContext.PARA_BANK_LOGO['class'], self.page.para_bank_logo_img_class)
-		self.assertEqual(HomePageContext.PARA_BANK_LOGO['src'], self.page.para_bank_logo_formated_img_src)
-		self.assertEqual(HomePageContext.PARA_BANK_LOGO['alt'], self.page.para_bank_logo_img_alt)
-		self.assertEqual(HomePageContext.PARA_BANK_LOGO['title'], self.page.para_bank_logo_img_title)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['href'],
+		                 self.page.para_bank_logo_formated_href)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['class'],
+		                 self.page.para_bank_logo_img_class)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['src'],
+		                 self.page.para_bank_logo_formated_img_src)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['alt'],
+		                 self.page.para_bank_logo_img_alt)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['title'],
+		                 self.page.para_bank_logo_img_title)
 
-		self.assertEqual(HomePageContext.MENU_BUTTONS['home']['href'], self.page.home_button_formated_href)
-		self.assertEqual(HomePageContext.MENU_BUTTONS['home']['text'], self.page.home_button_text)
+		self.assertEqual(HomePageContext.MENU_BUTTONS['home']['href'],
+		                 self.page.home_button_formated_href)
+		self.assertEqual(HomePageContext.MENU_BUTTONS['home']['text'],
+		                 self.page.home_button_text)
+
+		self.assertEqual(HomePageContext.MENU_BUTTONS['about']['href'],
+		                 self.page.about_button_formated_href)
+		self.assertEqual(HomePageContext.MENU_BUTTONS['about']['text'],
+		                 self.page.about_button_text)
+
+		self.assertEqual(HomePageContext.MENU_BUTTONS['contact']['href'],
+		                 self.page.contact_button_formated_href)
+		self.assertEqual(HomePageContext.MENU_BUTTONS['contact']['text'],
+		                 self.page.contact_button_text)
 
