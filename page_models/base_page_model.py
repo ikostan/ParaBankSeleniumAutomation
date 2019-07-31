e6ffb6556656bd7b1c3f127948f49b0bb4b5d9cd
Egor Kostan <igorkostan@gmail.com>
2019-07-30 21:19:11 -0700

    Update base_page_model.py

--- page_models/base_page_model.py
+++ page_models/base_page_model.py
@@ -111,7 +111,7 @@ class BasePageModel:
 		return element.text
 
 	@property
-	def admin_logo_href(self):
+	def _admin_logo_href(self):
 		'''
 		Returns href value from "Admin Logo" element
 		:return:
@@ -125,7 +125,51 @@ class BasePageModel:
 		Returns href value from "Admin Logo" element
 		:return:
 		'''
-		href = self.admin_logo_href
-		href = href.replace(BasePageContext.ROOT_URL, '')
-		href = href[:href.index(';')]
+		href = self._formated_url(self._admin_logo_href)
 		return href
+
+	@property
+	def _admin_logo_img_src(self):
+		'''
+		Returns admin logo img src value
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.ADMIN_LOGO_IMG)
+		return element.img_src
+
+	@property
+	def admin_logo_formated_img_src(self):
+		'''
+		Returns admin logo img src value
+		:return:
+		'''
+		src = self._formated_url(self._admin_logo_img_src)
+		return src
+
+	@staticmethod
+	def _formated_url(url):
+		'''
+		Removes following from href/url:
+		
+		1. https://parabank.parasoft.com/parabank
+		2. ;jsessionid=1219DE07E03D513449C6E1EEA9A73C43
+		
+		Result: /parabank/images/clear.gif
+		
+		:param url: 
+		:return: 
+		'''
+		url = url.replace(BasePageContext.ROOT_URL, '')
+		url = url[:url.index(';')]
+		return url
+
+	@property
+	def admin_logo_img_class(self):
+		'''
+		Returns admin logo img src value
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.ADMIN_LOGO_IMG)
+		return element.img_class
+
+
