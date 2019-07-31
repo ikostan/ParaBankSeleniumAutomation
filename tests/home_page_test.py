05b409d267b73709422b6e13e3c7e6448673d5fb
Egor Kostan <igorkostan@gmail.com>
2019-07-30 22:42:03 -0700

    Update home_page_test.py

--- tests/home_page_test.py
+++ tests/home_page_test.py
@@ -11,14 +11,22 @@ class HomePageTestCase(BaseTestCase):
 		# Open web page
 		browser = 'chrome'
 		self.driver = Driver(browser)
-		self.page = HomePageModel(self.driver)
+		self.page = HomePageModel(driver=self.driver, implicit_wait_time=5)
 		self.page.go()
 
 		# Test base context:
 		self.assertEqual(HomePageContext.URL, self.page.url)
 		self.assertEqual(HomePageContext.SLOGAN, self.page.slogan)
 		self.assertEqual(HomePageContext.TITLE, self.page.title)
+
 		self.assertEqual(HomePageContext.ADMIN_LOGO['href'], self.page.admin_logo_formated_href)
 		self.assertEqual(HomePageContext.ADMIN_LOGO['class'], self.page.admin_logo_img_class)
 		self.assertEqual(HomePageContext.ADMIN_LOGO['src'], self.page.admin_logo_formated_img_src)
+
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['href'], self.page.para_bank_logo_formated_href)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['class'], self.page.para_bank_logo_img_class)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['src'], self.page.para_bank_logo_formated_img_src)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['alt'], self.page.para_bank_logo_img_alt)
+		self.assertEqual(HomePageContext.PARA_BANK_LOGO['title'], self.page.para_bank_logo_img_title)
+
 
