8f6601acfa05de71a8606b76eda971732486db49
Egor Kostan <igorkostan@gmail.com>
2019-07-30 17:42:08 -0700

    Update home_page_test.py

--- tests/home_page_test.py
+++ tests/home_page_test.py
@@ -1,12 +1,12 @@
-from tests.base_test import BaseTestCase
 from utils.driver import Driver
+from tests.base_test import BaseTestCase
 from page_models.home_page_model import HomePageModel
 from page_context.home_page_context import HomePageContext
 
 
 class MyTestCase(BaseTestCase):
 
-	def test_something(self):
+	def test_home_page_is_up(self):
 		browser = 'chrome'
 		self.driver = Driver(browser)
 		self.page = HomePageModel(self.driver)
