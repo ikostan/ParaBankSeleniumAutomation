839cd82de71ebb1b8b86d75e7c0b7cd27ef01865
Egor Kostan <igorkostan@gmail.com>
2019-07-30 19:38:23 -0700

    Update home_page_locator.py

--- page_locators/home_page_locator.py
+++ page_locators/home_page_locator.py
@@ -1,9 +1,9 @@
-from selenium.webdriver.common.by import By
+from page_locators.base_page_locator import BasePageLocator
 
 
-class HomePageLocator:
+class HomePageLocator(BasePageLocator):
 	'''
-	Holds all relevant locators for HOME page web elements.
+	Holds all relevant locators for 'HOME' page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
 	'''
