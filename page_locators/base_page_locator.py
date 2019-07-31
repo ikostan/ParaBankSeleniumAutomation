89911d498fc2385fe52cb98af4762ef2722ab366
Egor Kostan <igorkostan@gmail.com>
2019-07-30 19:38:30 -0700

    Update base_page_locator.py

--- page_locators/base_page_locator.py
+++ page_locators/base_page_locator.py
@@ -3,8 +3,11 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocator:
 	'''
-	Holds all relevant locators for page web elements.
+	Holds all relevant locators for any page web elements.
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
 	'''
-	pass
+
+	SLOGAN = (By.XPATH, '//*[@id="topPanel"]/p')
+	ADMIN_LOGO_HREF = (By.CSS_SELECTOR, 'div[id="topPanel"] > a[href="/parabank/admin.htm"]')
+	ADMIN_LOGO_IMG = (By.XPATH, '//*[@id="topPanel"]/a[1]/img')
