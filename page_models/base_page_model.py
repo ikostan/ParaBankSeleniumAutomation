fddf7614e2d07a2585da13d6e80d160bea888540
Egor Kostan <igorkostan@gmail.com>
2019-07-30 22:42:22 -0700

    Update base_page_model.py

--- page_models/base_page_model.py
+++ page_models/base_page_model.py
@@ -110,6 +110,23 @@ class BasePageModel:
 		element = Element(self.driver, BasePageLocator.SLOGAN)
 		return element.text
 
+	@staticmethod
+	def _formated_url(url):
+		'''
+		Removes following from href/url:
+
+		1. https://parabank.parasoft.com/parabank
+		2. ;jsessionid=1219DE07E03D513449C6E1EEA9A73C43
+
+		Result: /parabank/images/clear.gif
+
+		:param url:
+		:return:
+		'''
+		url = url.replace(BasePageContext.ROOT_URL, '')
+		url = url[:url.index(';')]
+		return url
+
 	@property
 	def _admin_logo_href(self):
 		'''
@@ -117,7 +134,7 @@ class BasePageModel:
 		:return:
 		'''
 		element = Element(self.driver, BasePageLocator.ADMIN_LOGO_HREF)
-		return element.href
+		return element.element_href
 
 	@property
 	def admin_logo_formated_href(self):
@@ -135,7 +152,7 @@ class BasePageModel:
 		:return:
 		'''
 		element = Element(self.driver, BasePageLocator.ADMIN_LOGO_IMG)
-		return element.img_src
+		return element.element_src
 
 	@property
 	def admin_logo_formated_img_src(self):
@@ -146,23 +163,6 @@ class BasePageModel:
 		src = self._formated_url(self._admin_logo_img_src)
 		return src
 
-	@staticmethod
-	def _formated_url(url):
-		'''
-		Removes following from href/url:
-		
-		1. https://parabank.parasoft.com/parabank
-		2. ;jsessionid=1219DE07E03D513449C6E1EEA9A73C43
-		
-		Result: /parabank/images/clear.gif
-		
-		:param url: 
-		:return: 
-		'''
-		url = url.replace(BasePageContext.ROOT_URL, '')
-		url = url[:url.index(';')]
-		return url
-
 	@property
 	def admin_logo_img_class(self):
 		'''
@@ -170,6 +170,67 @@ class BasePageModel:
 		:return:
 		'''
 		element = Element(self.driver, BasePageLocator.ADMIN_LOGO_IMG)
-		return element.img_class
+		return element.element_class
 
+	@property
+	def _para_bank_logo_href(self):
+		'''
+		Returns href value from "ParBank Logo" element
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.PARA_BANK_LOGO_HREF)
+		return element.element_href
 
+	@property
+	def para_bank_logo_formated_href(self):
+		'''
+		Returns href value from "ParBank Logo" element
+		:return:
+		'''
+		href = self._formated_url(self._para_bank_logo_href)
+		return href
+
+	@property
+	def _para_bank_logo_img_src(self):
+		'''
+		Returns ParBank logo img src value
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.PARA_BANK_LOGO_IMG)
+		return element.element_src
+
+	@property
+	def para_bank_logo_formated_img_src(self):
+		'''
+		Returns ParBank logo img src value
+		:return:
+		'''
+		src = self._formated_url(self._para_bank_logo_img_src)
+		return src
+
+	@property
+	def para_bank_logo_img_class(self):
+		'''
+		Returns ParBank logo img class value
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.PARA_BANK_LOGO_IMG)
+		return element.element_class
+
+	@property
+	def para_bank_logo_img_alt(self):
+		'''
+		Returns ParBank logo img alt value
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.PARA_BANK_LOGO_IMG)
+		return element.element_alt
+
+	@property
+	def para_bank_logo_img_title(self):
+		'''
+		Returns ParBank logo img title value
+		:return:
+		'''
+		element = Element(self.driver, BasePageLocator.PARA_BANK_LOGO_IMG)
+		return element.element_title
