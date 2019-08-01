63a1e918dd5a6ea6251d693256afb4cdc7043c6f
Egor Kostan <igorkostan@gmail.com>
2019-07-31 18:13:42 -0700

    Update base_page_locator.py

--- page_locators/base_page_locator.py
+++ page_locators/base_page_locator.py
@@ -13,21 +13,36 @@ class BasePageLocator:
 	ADMIN_LOGO_IMG = (By.XPATH, '//*[@id="topPanel"]/a[contains(@href,"/parabank/admin.htm")]/img')
 
 	# ParaBank Logo:
-	PARA_BANK_LOGO_HREF = (By.XPATH, '//*[@id="topPanel"]/a[contains(@href,"/parabank/index.htm")]')
+	PARA_BANK_LOGO_HREF = (By.XPATH, '//*[@id="topPanel"]/'
+	                                 'a[contains(@href,"/'
+	                                 'parabank/index.htm")]')
+
 	PARA_BANK_LOGO_IMG = (By.XPATH, '//*[@id="topPanel"]/'
-	                                'a[contains(@href,"/parabank/index.htm")]/'
+	                                'a[contains(@href,"/'
+	                                'parabank/index.htm")]/'
 	                                'img[contains(@class,"logo")]')
-	SLOGAN = (By.XPATH, '//*[@id="topPanel"]/p[contains(@class,"caption")]')
+
+	SLOGAN = (By.XPATH, '//*[@id="topPanel"]/'
+	                    'p[contains(@class,"caption")]')
 
 	# Menu Buttons:
-	HOME_BUTTON = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"button")]/li[contains(@class,"home")]/a')
-	ABOUT_BUTTON = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"button")]/li[contains(@class,"aboutus")]/a')
-	CONTACT_BUTTON = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"button")]/li[contains(@class,"contact")]/a')
+	HOME_BUTTON = (By.XPATH, '//*[@id="headerPanel"]/'
+	                         'ul[contains(@class,"button")]/'
+	                         'li[contains(@class,"home")]/a')
+
+	ABOUT_BUTTON = (By.XPATH, '//*[@id="headerPanel"]/'
+	                          'ul[contains(@class,"button")]/'
+	                          'li[contains(@class,"aboutus")]/a')
+
+	CONTACT_BUTTON = (By.XPATH, '//*[@id="headerPanel"]/'
+	                            'ul[contains(@class,"button")]/'
+	                            'li[contains(@class,"contact")]/a')
 
 	# Left Menu:
 	SOLUTIONS = (By.XPATH, '//*[@id="headerPanel"]/'
 	                       'ul[contains(@class,"leftmenu")]/'
 	                       'li[contains(@class,"Solutions")]')
+
 	ABOUT_US_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/'
 	                                'ul[contains(@class,"leftmenu")]/'
 	                                'li[2]/'
@@ -38,10 +53,18 @@ class BasePageLocator:
 	                                'li[3]/'
 	                                'a[contains(@href, "services.htm")]')
 
-	PRODUCTS_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"leftmenu")]/li[4]/a[contains(@href, "http://www.parasoft.com/jsp/products.jsp")]')
-	#PRODUCTS_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"leftmenu")]/li[4]/a')
+	PRODUCTS_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/'
+	                                'ul[contains(@class,"leftmenu")]/'
+	                                'li[4]/'
+	                                'a[contains(@href, "http://www.parasoft.com/jsp/products.jsp")]')
 
-	LOCATIONS_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"leftmenu")]/li[5]/a[contains(@href, "http://www.parasoft.com/jsp/pr/contacts.jsp")]')
+	LOCATIONS_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/'
+	                                 'ul[contains(@class,"leftmenu")]/'
+	                                 'li[5]/'
+	                                 'a[contains(@href, "http://www.parasoft.com/jsp/pr/contacts.jsp")]')
 
-	ADMIN_PAGE_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/ul[contains(@class,"leftmenu")]/li[6]/a[contains(@href, "admin.htm")]')
+	ADMIN_PAGE_MENU_ITEM = (By.XPATH, '//*[@id="headerPanel"]/'
+	                                  'ul[contains(@class,"leftmenu")]/'
+	                                  'li[6]/'
+	                                  'a[contains(@href, "admin.htm")]')
 
