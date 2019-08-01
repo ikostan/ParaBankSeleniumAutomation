490258dfbcdce70ee988055fa7e13eeb597c9242
Egor Kostan <igorkostan@gmail.com>
2019-07-31 18:13:46 -0700

    Update base_test.py

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,5 +1,6 @@
 import unittest
 from utils.screenshot import screenshots_collector
+from page_context.base_page_context import BasePageContext
 
 
 class BaseTestCase(unittest.TestCase):
@@ -28,6 +29,75 @@ class BaseTestCase(unittest.TestCase):
 		screenshots_collector()
 		if self.page:
 			self.page.close()
+
+	def verify_solutions_menu_items(self):
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Solutions']['text'],
+		                 self.page.solutions_menu_text)
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Solutions']['class'],
+		                 self.page.solutions_menu_class)
+
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['About Us']['href'],
+		                 self.page.about_us_menu_item_formated_href)
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['About Us']['text'],
+		                 self.page.about_us_menu_item_text)
+
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Services']['href'],
+		                 self.page.services_us_menu_item_formated_href)
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Services']['text'],
+		                 self.page.services_us_menu_item_text)
+
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Products']['href'],
+		                 self.page.products_menu_item_formated_href)
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Products']['text'],
+		                 self.page.products_menu_item_text)
+
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Locations']['href'],
+		                 self.page.locations_menu_item_formated_href)
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Locations']['text'],
+		                 self.page.locations_menu_item_text)
+
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Admin Page']['href'],
+		                 self.page.admin_page_menu_item_formated_href)
+		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Admin Page']['text'],
+		                 self.page.admin_page_menu_item_text)
+
+	def verify_right_menu_buttons(self):
+		self.assertEqual(BasePageContext.MENU_BUTTONS['home']['href'],
+		                 self.page.home_button_formated_href)
+		self.assertEqual(BasePageContext.MENU_BUTTONS['home']['text'],
+		                 self.page.home_button_text)
+
+		self.assertEqual(BasePageContext.MENU_BUTTONS['about']['href'],
+		                 self.page.about_button_formated_href)
+		self.assertEqual(BasePageContext.MENU_BUTTONS['about']['text'],
+		                 self.page.about_button_text)
+
+		self.assertEqual(BasePageContext.MENU_BUTTONS['contact']['href'],
+		                 self.page.contact_button_formated_href)
+		self.assertEqual(BasePageContext.MENU_BUTTONS['contact']['text'],
+		                 self.page.contact_button_text)
+
+	def verify_parabank_logo(self):
+		self.assertEqual(BasePageContext.SLOGAN,
+		                 self.page.slogan)
+		self.assertEqual(BasePageContext.PARA_BANK_LOGO['href'],
+		                 self.page.para_bank_logo_formated_href)
+		self.assertEqual(BasePageContext.PARA_BANK_LOGO['class'],
+		                 self.page.para_bank_logo_img_class)
+		self.assertEqual(BasePageContext.PARA_BANK_LOGO['src'],
+		                 self.page.para_bank_logo_formated_img_src)
+		self.assertEqual(BasePageContext.PARA_BANK_LOGO['alt'],
+		                 self.page.para_bank_logo_img_alt)
+		self.assertEqual(BasePageContext.PARA_BANK_LOGO['title'],
+		                 self.page.para_bank_logo_img_title)
+
+	def verify_parabank_admin_logo(self):
+		self.assertEqual(BasePageContext.ADMIN_LOGO['href'],
+		                 self.page.admin_logo_formated_href)
+		self.assertEqual(BasePageContext.ADMIN_LOGO['class'],
+		                 self.page.admin_logo_img_class)
+		self.assertEqual(BasePageContext.ADMIN_LOGO['src'],
+		                 self.page.admin_logo_formated_img_src)
 
 
 if __name__ == '__main__':
