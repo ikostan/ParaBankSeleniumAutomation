93cc3babe14f05185f7d3dfdb1a00f38a340cccd
Egor Kostan <igorkostan@gmail.com>
2019-08-02 14:09:44 -0700

    Update chrome_home_page_base_context_test.py

--- tests/chrome/chrome_home_page_base_context_test.py
+++ tests/chrome/chrome_home_page_base_context_test.py
@@ -107,6 +107,7 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 	@allure.feature("Home Page Context.")
 	@allure.story("Welcome to ParaBank (Right Menu) > About button verification")
 	@allure.severity(severity_level.MINOR)
+	@allure.step("Verify Home button properties")
 	def test_right_menu_about_button(self):
 		# open web browser
 		self.open_web_browser(self.browser)
