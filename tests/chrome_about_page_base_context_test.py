18e6e0598126955cc71f689f3d1185fad041a890
Egor Kostan <igorkostan@gmail.com>
2019-08-01 17:27:44 -0700

    Create chrome_about_page_base_context_test.py

---
+++ tests/chrome_about_page_base_context_test.py
@@ -0,0 +1,60 @@
+from utils.screenshot import screenshot_on_fail
+from tests.base_context.about_page_context_case import AboutPageContextTestCase
+
+
+@screenshot_on_fail()
+class AboutPageContextTestCase(AboutPageContextTestCase):
+
+	browser = 'chrome'
+
+	def test_page_url_title(self):
+
+		# open web browser
+		self.open_web_browser(self.browser)
+		self.verify_page_url_title()
+
+	def test_parabank_admin_logo(self):
+
+		# open web browser
+		self.open_web_browser(self.browser)
+
+		# Context base elements validation:
+		self.verify_parabank_admin_logo()
+
+	def test_parabank_logo(self):
+		# open web browser
+		self.open_web_browser(self.browser)
+
+		# Context base elements validation:
+		self.verify_parabank_logo()
+
+	def test_right_menu(self):
+		# open web browser
+		self.open_web_browser(self.browser)
+
+		# Context base elements validation:
+		self.verify_right_menu_buttons()
+
+	def test_solutions_menu_items(self):
+		# open web browser
+		self.open_web_browser(self.browser)
+
+		# Context base elements validation:
+		self.verify_solutions_menu_items()
+
+	def test_customer_login(self):
+		# open web browser
+		self.open_web_browser(self.browser)
+
+		# Context base elements validation:
+		self.verify_customer_login()
+
+	def test_footer_items(self):
+
+		# open web browser
+		self.open_web_browser(self.browser)
+
+		# Context base elements validation:
+		self.verify_footer_items()
+
+
