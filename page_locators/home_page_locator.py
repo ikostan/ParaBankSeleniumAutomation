e53a27ad9c0b606612e447bc007b0d717e1212c3
Egor Kostan <igorkostan@gmail.com>
2019-08-01 20:51:05 -0700

    Update home_page_locator.py

--- page_locators/home_page_locator.py
+++ page_locators/home_page_locator.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from page_locators.base_page_locator import BasePageLocator
 
 
@@ -7,4 +8,24 @@ class HomePageLocator(BasePageLocator):
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
 	'''
-	pass
+
+	ATM_SERVICES_TITLE = (By.XPATH, '//*[@id="rightPanel"]/'
+	                                'ul[contains(@class, "services")]/'
+	                                'li[contains(@class, "captionone")]')
+
+	WITHDRAW_FUNDS = (By.XPATH, '//*[@id="rightPanel"]/'
+	                            'ul[contains(@class, "services")]/'
+	                            'li[2]/a')
+
+	TRANSFER_FUNDS = (By.XPATH, '//*[@id="rightPanel"]/'
+	                            'ul[contains(@class, "services")]/'
+	                            'li[3]/a')
+
+	CHECK_BALANCES = (By.XPATH, '//*[@id="rightPanel"]/'
+	                            'ul[contains(@class, "services")]/'
+	                            'li[4]/a')
+
+	MAKE_DEPOSITS = (By.XPATH, '//*[@id="rightPanel"]/'
+	                           'ul[contains(@class, "services")]/'
+	                           'li[5]/a')
+
