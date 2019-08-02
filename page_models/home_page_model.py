b6091f5eb06c2a91fdf609d4d34b7e1901297bd9
Egor Kostan <igorkostan@gmail.com>
2019-08-01 20:50:59 -0700

    Update home_page_model.py

--- page_models/home_page_model.py
+++ page_models/home_page_model.py
@@ -1,5 +1,7 @@
+from elements.element import Element
 from page_models.base_page_model import BasePageModel
 from page_context.home_page_context import HomePageContext
+from page_locators.home_page_locator import HomePageLocator
 
 
 class HomePageModel(BasePageModel):
@@ -9,3 +11,54 @@ class HomePageModel(BasePageModel):
 	'''
 
 	_url = HomePageContext.URL
+
+	@property
+	def atm_title(self):
+		'''
+		Returns title from ATM Services section
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.ATM_SERVICES_TITLE)
+		txt = element.text
+		return txt
+
+	@property
+	def atm_withdraw_funds_text(self):
+		'''
+		Returns withdraw_funds_text from ATM Services section
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.WITHDRAW_FUNDS)
+		txt = element.text
+		return txt
+
+	@property
+	def atm_withdraw_funds_formated_href(self):
+		'''
+		Returns withdraw_funds_href from ATM Services section
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.WITHDRAW_FUNDS)
+		href = super()._formated_url(element.element_href)
+		return href
+
+	@property
+	def atm_transfer_funds_text(self):
+		'''
+		Returns transfer_funds_text from ATM Services section
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.TRANSFER_FUNDS)
+		txt = element.text
+		return txt
+
+	@property
+	def atm_transfer_funds_formated_href(self):
+		'''
+		Returns transfer_funds_href from ATM Services section
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, HomePageLocator.TRANSFER_FUNDS)
+		href = super()._formated_url(element.element_href)
+		return href
+
