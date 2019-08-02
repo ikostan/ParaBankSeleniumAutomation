9a3fe16ac927bdaf822c343fa7a597c03ec568ed
Egor Kostan <igorkostan@gmail.com>
2019-08-01 17:32:08 -0700

    Create services_page_locator.py

---
+++ page_locators/services_page_locator.py
@@ -0,0 +1,10 @@
+from page_locators.base_page_locator import BasePageLocator
+
+
+class ServicesPageLocator(BasePageLocator):
+	'''
+	Holds all relevant locators for 'Services' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
+	pass
