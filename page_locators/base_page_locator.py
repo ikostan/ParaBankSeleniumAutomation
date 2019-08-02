634dd457bbee316d94bdc9ac0630475952d8b1f4
Egor Kostan <igorkostan@gmail.com>
2019-08-01 17:25:00 -0700

    Update base_page_locator.py

--- page_locators/base_page_locator.py
+++ page_locators/base_page_locator.py
@@ -9,8 +9,11 @@ class BasePageLocator:
 	'''
 
 	# Admin Logo:
-	ADMIN_LOGO_HREF = (By.XPATH, '//*[@id="topPanel"]/a[contains(@href,"/parabank/admin.htm")]')
-	ADMIN_LOGO_IMG = (By.XPATH, '//*[@id="topPanel"]/a[contains(@href,"/parabank/admin.htm")]/img')
+	ADMIN_LOGO_HREF = (By.XPATH, '//*[@id="topPanel"]/'
+	                             'a[contains(@href,"/parabank/admin.htm")]')
+
+	ADMIN_LOGO_IMG = (By.XPATH, '//*[@id="topPanel"]/'
+	                            'a[contains(@href,"/parabank/admin.htm")]/img')
 
 	# ParaBank Logo:
 	PARA_BANK_LOGO_HREF = (By.XPATH, '//*[@id="topPanel"]/'
@@ -71,13 +74,17 @@ class BasePageLocator:
 	# Customer Login
 	CUSTOMER_LOGIN_TITLE = (By.XPATH, '//*[@id="leftPanel"]/h2')
 	# Username
-	USERNAME_LOGIN_TITLE = (By.XPATH, '//*[@id="loginPanel"]/form/p[1]/b')
+	USERNAME_LOGIN_TITLE = (By.XPATH, '//*[@id="loginPanel"]/'
+	                                  'form/p[1]/b')
+
 	LOGIN_USERNAME_INPUT = (By.XPATH, '//*[@id="loginPanel"]/'
 	                                  'form/'
 	                                  'div[contains(@class, "login")]/'
 	                                  'input[contains(@name, "username")]')
 	# Password
-	PASSWORD_LOGIN_TITLE = (By.XPATH, '//*[@id="loginPanel"]/form/p[2]/b')
+	PASSWORD_LOGIN_TITLE = (By.XPATH, '//*[@id="loginPanel"]/'
+	                                  'form/p[2]/b')
+
 	LOGIN_PASSWORD_INPUT = (By.XPATH, '//*[@id="loginPanel"]/'
 	                                  'form/'
 	                                  'div[contains(@class, "login")]/'
@@ -88,20 +95,48 @@ class BasePageLocator:
 	                                   'div[contains(@class, "login")]/'
 	                                   'input[contains(@type, "submit")]')
 
-	FORGOT_LOGIN = (By.XPATH, '//*[@id="loginPanel"]/p/a[contains(@href, "lookup.htm")]')
-	REGISTER = (By.XPATH, '//*[@id="loginPanel"]/p/a[contains(@href, "register.htm")]')
+	FORGOT_LOGIN = (By.XPATH, '//*[@id="loginPanel"]/p/'
+	                          'a[contains(@href, "lookup.htm")]')
+
+	REGISTER = (By.XPATH, '//*[@id="loginPanel"]/p/'
+	                      'a[contains(@href, "register.htm")]')
 
 	# Footer
 	FOOTER_HOME = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[1]/'
 	                         'a[contains(@href, "parabank/index.htm")]')
-	FOOTER_ABOUT_US = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[2]/a[contains(@href, "about.htm")]')
-	FOOTER_SERVICES = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[3]/a[contains(@href, "services.htm")]')
-	FOOTER_PRODUCTS = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[4]/a[contains(@href, "http://www.parasoft.com/jsp/products.jsp")]')
-	FOOTER_LOCATIONS = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[5]/a[contains(@href, "http://www.parasoft.com/jsp/contacts.jsp")]')
-	FOOTER_FORUM = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[6]/a[contains(@href, "http://www.parasoft.com/")]')
-	FOOTER_SITE_MAP = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[7]/a[contains(@href, "parabank/sitemap.htm")]')
-	FOOTER_CONTACT_US = (By.XPATH, '//*[@id="footerPanel"]/ul[1]/li[8]/a[contains(@href, "contact.htm")]')
 
-	FOOTER_COPYRIGHT = (By.XPATH, '//*[@id="footerPanel"]/p[contains(@class, "copyright")]')
+	FOOTER_ABOUT_US = (By.XPATH, '//*[@id="footerPanel"]/'
+	                             'ul[1]/li[2]/'
+	                             'a[contains(@href, "about.htm")]')
+
+	FOOTER_SERVICES = (By.XPATH, '//*[@id="footerPanel"]/'
+	                             'ul[1]/li[3]/'
+	                             'a[contains(@href, "services.htm")]')
+
+	FOOTER_PRODUCTS = (By.XPATH, '//*[@id="footerPanel"]/'
+	                             'ul[1]/li[4]/'
+	                             'a[contains(@href, "http://www.parasoft.com/jsp/products.jsp")]')
+
+	FOOTER_LOCATIONS = (By.XPATH, '//*[@id="footerPanel"]/'
+	                              'ul[1]/li[5]/a')
+
+	FOOTER_FORUM = (By.XPATH, '//*[@id="footerPanel"]/'
+	                          'ul[1]/li[6]/'
+	                          'a[contains(@href, "http://forums.parasoft.com/")]')
+
+	FOOTER_SITE_MAP = (By.XPATH, '//*[@id="footerPanel"]/'
+	                             'ul[1]/li[7]/'
+	                             'a[contains(@href, "parabank/sitemap.htm")]')
+
+	FOOTER_CONTACT_US = (By.XPATH, '//*[@id="footerPanel"]/'
+	                               'ul[1]/li[8]/'
+	                               'a[contains(@href, "contact.htm")]')
+
+	FOOTER_COPYRIGHT = (By.XPATH, '//*[@id="footerPanel"]/'
+	                              'p[contains(@class, "copyright")]')
+
 	FOOTER_VISIT_US = (By.XPATH, '//*[@id="footerPanel"]/ul[contains(@class, "visit")]/li[1]')
-	FOOTER_VISIT_US_LINK = (By.XPATH, '//*[@id="footerPanel"]/ul[contains(@class, "visit")]/li/a[contains(@href, "http://www.parasoft.com/")]')
+
+	FOOTER_VISIT_US_LINK = (By.XPATH, '//*[@id="footerPanel"]/'
+	                                  'ul[contains(@class, "visit")]/li/'
+	                                  'a[contains(@href, "http://www.parasoft.com/")]')
