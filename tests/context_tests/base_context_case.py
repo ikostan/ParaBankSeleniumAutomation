7710cf6f7ba07ecfff96d47aa3c58224de0c73dd
Egor Kostan <igorkostan@gmail.com>
2019-08-03 16:57:21 -0700

    Update base_context_case.py

--- tests/context_tests/base_context_case.py
+++ tests/context_tests/base_context_case.py
@@ -20,7 +20,7 @@ class TestBaseContextCase(unittest.TestCase):
 
 	def setUp(self):
 		with allure.step("""
-		Test setUp: 
+		setUp: 
 			1. Close current browser tab. 
 			2. Set webdriver to None.
 		"""):
@@ -34,41 +34,49 @@ class TestBaseContextCase(unittest.TestCase):
 			cls.page.quit()
 
 	def tearDown(self):
-		with allure.step("Test tearDown: close current browser tab"):
+		with allure.step("tearDown: close current browser tab"):
 			# screenshots_collector()
 			if self.page:
 				self.page.close()
 
 	def solutions_menu_items_test(self):
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Solutions']['text'],
-		                 self.page.solutions_menu_text)
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Solutions']['class'],
-		                 self.page.solutions_menu_class)
 
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['About Us']['href'],
-		                 self.page.about_us_menu_item_formated_href)
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['About Us']['text'],
-		                 self.page.about_us_menu_item_text)
+		with allure.step("Test 'Solutions' menu title: text + css"):
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Solutions']['text'],
+			                 self.page.solutions_menu_text)
 
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Services']['href'],
-		                 self.page.services_us_menu_item_formated_href)
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Services']['text'],
-		                 self.page.services_us_menu_item_text)
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Solutions']['class'],
+			                 self.page.solutions_menu_class)
 
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Products']['href'],
-		                 self.page.products_menu_item_formated_href)
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Products']['text'],
-		                 self.page.products_menu_item_text)
+		with allure.step("Test 'Solutions' menu > 'About Us': text + href"):
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['About Us']['href'],
+			                 self.page.about_us_menu_item_formated_href)
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['About Us']['text'],
+			                 self.page.about_us_menu_item_text)
 
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Locations']['href'],
-		                 self.page.locations_menu_item_formated_href)
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Locations']['text'],
-		                 self.page.locations_menu_item_text)
+		with allure.step("Test 'Solutions' menu > 'Services': text + href"):
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Services']['href'],
+			                 self.page.services_us_menu_item_formated_href)
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Services']['text'],
+			                 self.page.services_us_menu_item_text)
 
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Admin Page']['href'],
-		                 self.page.admin_page_menu_item_formated_href)
-		self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Admin Page']['text'],
-		                 self.page.admin_page_menu_item_text)
+		with allure.step("Test 'Solutions' menu > 'Products': text + href"):
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Products']['href'],
+			                 self.page.products_menu_item_formated_href)
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Products']['text'],
+			                 self.page.products_menu_item_text)
+
+		with allure.step("Test 'Solutions' menu > 'Locations': text + href"):
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Locations']['href'],
+			                 self.page.locations_menu_item_formated_href)
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Locations']['text'],
+			                 self.page.locations_menu_item_text)
+
+		with allure.step("Test 'Solutions' menu > 'Admin Page': text + href"):
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Admin Page']['href'],
+			                 self.page.admin_page_menu_item_formated_href)
+			self.assertEqual(BasePageContext.LEFT_MENU_ITEMS['Admin Page']['text'],
+			                 self.page.admin_page_menu_item_text)
 
 	def right_menu_home_button_test(self):
 
@@ -76,16 +84,19 @@ class TestBaseContextCase(unittest.TestCase):
 				BasePageContext.MENU_BUTTONS['home']['href'])):
 			self.assertEqual(BasePageContext.MENU_BUTTONS['home']['href'],
 			                 self.page.home_button_formated_href)
+
 		with allure.step('Do "Home" button verification > verify button label. Expected: {}.'.format(
 				BasePageContext.MENU_BUTTONS['home']['text'])):
 			self.assertEqual(BasePageContext.MENU_BUTTONS['home']['text'],
 			                 self.page.home_button_text)
 
 	def right_menu_about_button_test(self):
+
 		with allure.step('Do "About" button verification > verify button href. Expected: {}.'.format(
 				BasePageContext.MENU_BUTTONS['about']['href'])):
 			self.assertEqual(BasePageContext.MENU_BUTTONS['about']['href'],
 			                 self.page.about_button_formated_href)
+
 		with allure.step('Do "About" button verification > verify button label. Expected: {}.'.format(
 				BasePageContext.MENU_BUTTONS['about']['text'])):
 			self.assertEqual(BasePageContext.MENU_BUTTONS['about']['text'],
@@ -97,32 +108,51 @@ class TestBaseContextCase(unittest.TestCase):
 				BasePageContext.MENU_BUTTONS['contact']['href'])):
 			self.assertEqual(BasePageContext.MENU_BUTTONS['contact']['href'],
 			                 self.page.contact_button_formated_href)
+
 		with allure.step('Do "Contact" button verification > verify button href. Expected: {}.'.format(
 				BasePageContext.MENU_BUTTONS['contact']['text'])):
 			self.assertEqual(BasePageContext.MENU_BUTTONS['contact']['text'],
 			                 self.page.contact_button_text)
 
 	def parabank_logo_test(self):
-		self.assertEqual(BasePageContext.SLOGAN,
-		                 self.page.slogan)
-		self.assertEqual(BasePageContext.PARA_BANK_LOGO['href'],
-		                 self.page.para_bank_logo_formated_href)
-		self.assertEqual(BasePageContext.PARA_BANK_LOGO['class'],
-		                 self.page.para_bank_logo_img_class)
-		self.assertEqual(BasePageContext.PARA_BANK_LOGO['src'],
-		                 self.page.para_bank_logo_formated_img_src)
-		self.assertEqual(BasePageContext.PARA_BANK_LOGO['alt'],
-		                 self.page.para_bank_logo_img_alt)
-		self.assertEqual(BasePageContext.PARA_BANK_LOGO['title'],
-		                 self.page.para_bank_logo_img_title)
+
+		with allure.step("Test 'ParaBank' logo > slogan"):
+			self.assertEqual(BasePageContext.SLOGAN,
+			                 self.page.slogan)
+
+		with allure.step("Test 'ParaBank' logo > href"):
+			self.assertEqual(BasePageContext.PARA_BANK_LOGO['href'],
+			                 self.page.para_bank_logo_formated_href)
+
+		with allure.step("Test 'ParaBank' logo > class"):
+			self.assertEqual(BasePageContext.PARA_BANK_LOGO['class'],
+			                 self.page.para_bank_logo_img_class)
+
+		with allure.step("Test 'ParaBank' logo > src"):
+			self.assertEqual(BasePageContext.PARA_BANK_LOGO['src'],
+			                 self.page.para_bank_logo_formated_img_src)
+
+		with allure.step("Test 'ParaBank' logo > alt"):
+			self.assertEqual(BasePageContext.PARA_BANK_LOGO['alt'],
+			                 self.page.para_bank_logo_img_alt)
+
+		with allure.step("Test 'ParaBank' logo > title"):
+			self.assertEqual(BasePageContext.PARA_BANK_LOGO['title'],
+			                 self.page.para_bank_logo_img_title)
 
 	def parabank_admin_logo_test(self):
-		self.assertEqual(BasePageContext.ADMIN_LOGO['href'],
-		                 self.page.admin_logo_formated_href)
-		self.assertEqual(BasePageContext.ADMIN_LOGO['class'],
-		                 self.page.admin_logo_img_class)
-		self.assertEqual(BasePageContext.ADMIN_LOGO['src'],
-		                 self.page.admin_logo_formated_img_src)
+
+		with allure.step("Test 'Admin' logo > href"):
+			self.assertEqual(BasePageContext.ADMIN_LOGO['href'],
+			                 self.page.admin_logo_formated_href)
+
+		with allure.step("Test 'Admin' logo > class"):
+			self.assertEqual(BasePageContext.ADMIN_LOGO['class'],
+			                 self.page.admin_logo_img_class)
+
+		with allure.step("Test 'Admin' logo > src"):
+			self.assertEqual(BasePageContext.ADMIN_LOGO['src'],
+			                 self.page.admin_logo_formated_img_src)
 
 	def customer_login_test(self):
 
@@ -170,12 +200,14 @@ class TestBaseContextCase(unittest.TestCase):
 		self.assertEqual(BasePageContext.REGISTER['text'],
 		                 self.page.register_text)
 
+	@allure.severity(allure.severity_level.TRIVIAL)
 	def footer_items_test(self):
 
-		self.assertEqual(BasePageContext.FOOTER['footer menu']['home']['href'],
-		                 self.page.footer_home_formated_href)
-		self.assertEqual(BasePageContext.FOOTER['footer menu']['home']['text'],
-		                 self.page.footer_home_text)
+		with allure.step("Test 'Footer' items > 'Home' > href + text"):
+			self.assertEqual(BasePageContext.FOOTER['footer menu']['home']['href'],
+			                 self.page.footer_home_formated_href)
+			self.assertEqual(BasePageContext.FOOTER['footer menu']['home']['text'],
+			                 self.page.footer_home_text)
 
 		self.assertEqual(BasePageContext.FOOTER['footer menu']['about us']['href'],
 		                 self.page.footer_about_us_formated_href)
