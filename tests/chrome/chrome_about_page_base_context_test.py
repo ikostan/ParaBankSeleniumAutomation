201bcc0bafcf230489f96e01c423d558141a1ef9
Egor Kostan <igorkostan@gmail.com>
2019-08-03 00:57:48 -0700

    Update chrome_about_page_base_context_test.py

--- tests/chrome/chrome_about_page_base_context_test.py
+++ tests/chrome/chrome_about_page_base_context_test.py
@@ -6,7 +6,7 @@ from tests.context_tests.about_page_context_case import AboutPageContextTestCase
 @allure.suite("Chrome Browser Context Testing")
 @allure.sub_suite('Chrome About Page Context Test')
 @screenshot_on_fail()
-class ChromeAboutPageContextTest(AboutPageContextTestCase):
+class TestChromeAboutPageContext(AboutPageContextTestCase):
 
 	browser = 'chrome'
 
