3497ac592bc4cc77894f63ce264243bcfa3a9552
Egor Kostan <igorkostan@gmail.com>
2019-08-02 18:11:33 -0700

    Update chrome_home_page_base_context_test.py

--- tests/chrome/chrome_home_page_base_context_test.py
+++ tests/chrome/chrome_home_page_base_context_test.py
@@ -1,78 +1,53 @@
 import allure
-from allure import severity_level
 from utils.screenshot import screenshot_on_fail
+from page_context.home_page_context import HomePageContext
+from allure_commons.types import Severity
 from tests.context_tests.home_page_context_case import HomePageContextTestCase
 
 
+@allure.epic("ParaBank Web App")
+@allure.feature("Home Page Context.")
 @screenshot_on_fail()
 class ChromeHomePageContextTestCase(HomePageContextTestCase):
 
 	browser = 'chrome'
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("ParaBank URL and Web Page Title verification")
-	@allure.severity(severity_level.MINOR)
 	def test_page_url_title(self):
 
 		# open web browser
 		self.open_web_browser(self.browser)
 		self.verify_page_url_title()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("ATM Services items verification")
-	@allure.severity(severity_level.MINOR)
 	def test_atm_services_context(self):
 
 		# open web browser
 		self.open_web_browser(self.browser)
 		self.verify_atm_services_context()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Online Services items verification")
-	@allure.severity(severity_level.MINOR)
 	def test_online_services_context(self):
 
 		# open web browser
 		self.open_web_browser(self.browser)
 		self.verify_online_services_context()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Services > Read More button verification")
-	@allure.severity(severity_level.MINOR)
 	def test_read_more_services_button(self):
 
 		# open web browser
 		self.open_web_browser(self.browser)
 		self.verify_read_more_services_button()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("News Services > Read More button verification")
-	@allure.severity(severity_level.MINOR)
 	def test_read_more_news_button(self):
 
 		# open web browser
 		self.open_web_browser(self.browser)
 		self.verify_read_more_news_button()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Latest News title verification")
-	@allure.severity(severity_level.MINOR)
 	def test_latest_news_title(self):
 
 		# open web browser
 		self.open_web_browser(self.browser)
 		self.verify_latest_news_title()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Admin Logo verification")
-	@allure.severity(severity_level.MINOR)
 	def test_parabank_admin_logo(self):
 
 		# open web browser
@@ -81,10 +56,6 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 		# Context base elements validation:
 		self.verify_parabank_admin_logo()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("ParaBank Logo verification")
-	@allure.severity(severity_level.MINOR)
 	def test_parabank_logo(self):
 		# open web browser
 		self.open_web_browser(self.browser)
@@ -92,10 +63,6 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 		# Context base elements validation:
 		self.verify_parabank_logo()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Welcome to ParaBank (Right Menu) > Home button verification")
-	@allure.severity(severity_level.MINOR)
 	def test_right_menu_home_button(self):
 		# open web browser
 		self.open_web_browser(self.browser)
@@ -103,23 +70,20 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 		# Context base elements validation:
 		self.right_menu_home_button_test()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
 	@allure.story("Welcome to ParaBank (Right Menu) > About button verification")
-	@allure.severity(severity_level.MINOR)
-	@allure.step("Verify Home button properties")
-	@allure.description("Home web page > Home button verification: href + text")
+	@allure.title("Test Home button attributes")
 	def test_right_menu_about_button(self):
+		"""Context base elements validation > Home button:
+		1. Open Home web page
+		2. Do Home button verification: href + text
+		"""
 		# open web browser
-		self.open_web_browser(self.browser)
-
+		with allure.step('Open test web page: {}.'.format(HomePageContext.URL)):
+			self.open_web_browser(self.browser)
 		# Context base elements validation:
-		self.right_menu_about_button_test()
+		with allure.step('Do Home button verification: href + text.'):
+			self.right_menu_about_button_test()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Welcome to ParaBank (Right Menu) > Contact button verification")
-	@allure.severity(severity_level.MINOR)
 	def test_right_menu_contact_button(self):
 		# open web browser
 		self.open_web_browser(self.browser)
@@ -127,10 +91,6 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 		# Context base elements validation:
 		self.right_menu_contact_button_test()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Solutions menu items verification")
-	@allure.severity(severity_level.NORMAL)
 	def test_solutions_menu_items(self):
 		# open web browser
 		self.open_web_browser(self.browser)
@@ -138,10 +98,6 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 		# Context base elements validation:
 		self.verify_solutions_menu_items()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Customer Login verification")
-	@allure.severity(severity_level.BLOCKER)
 	def test_customer_login(self):
 		# open web browser
 		self.open_web_browser(self.browser)
@@ -149,10 +105,6 @@ class ChromeHomePageContextTestCase(HomePageContextTestCase):
 		# Context base elements validation:
 		self.verify_customer_login()
 
-	@allure.epic("ParaBank Web App")
-	@allure.feature("Home Page Context.")
-	@allure.story("Footer menu items verification")
-	@allure.severity(severity_level.NORMAL)
 	def test_footer_items(self):
 
 		# open web browser
