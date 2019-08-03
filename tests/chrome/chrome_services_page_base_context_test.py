e9946b742fe73b54a763ca17fcca68a6569d2b1d
Egor Kostan <igorkostan@gmail.com>
2019-08-02 20:43:30 -0700

    Update chrome_services_page_base_context_test.py

--- tests/chrome/chrome_services_page_base_context_test.py
+++ tests/chrome/chrome_services_page_base_context_test.py
@@ -3,8 +3,8 @@ from utils.screenshot import screenshot_on_fail
 from tests.context_tests.services_page_context_case import ServicesPageContextTestCase
 
 
-@allure.epic("ParaBank Web App")
-@allure.feature("Services Page Context.")
+# @allure.epic("ParaBank Web App")
+@allure.suite("Chrome Browser Context Testing")
 @screenshot_on_fail()
 class ServicesPageContextTestCase(ServicesPageContextTestCase):
 
