2a35e6479902d32d715ddd217edd45c1cb523829
Egor Kostan <igorkostan@gmail.com>
2019-08-04 14:39:48 -0700

    Delete admin_page_context.py

--- page_context/admin_page_context.py
+++
@@ -1,8 +0,0 @@
-from page_context.base_page_context import BasePageContext
-
-
-class AdminPageContext(BasePageContext):
-
-	TITLE = BasePageContext.TITLE + 'Administration'
-
-	URL = BasePageContext.URL + 'admin.htm'
