8ad29891a2157b4fa0c7783011b06b4653333a65
Egor Kostan <igorkostan@gmail.com>
2019-08-04 14:39:09 -0700

    Create services_page_context.py

---
+++ expected_results/page_context/services_page_context.py
@@ -0,0 +1,8 @@
+from expected_results.page_context.base_page_context import BasePageContext
+
+
+class ServicesPageContext(BasePageContext):
+
+	TITLE = BasePageContext.TITLE + 'Services'
+
+	URL = BasePageContext.URL + 'services.htm'
