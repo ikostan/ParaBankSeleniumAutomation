d862ada1df91aa433f91edddd52b5307f3fe3248
Egor Kostan <igorkostan@gmail.com>
2019-08-04 21:30:01 -0700

    Update admin_page_context_case.py

--- tests/context_tests/admin_page_context_case.py
+++ tests/context_tests/admin_page_context_case.py
@@ -12,17 +12,24 @@ from expected_results.page_context.admin_page_context import AdminPageContext
 class AdminPageContextTestCase(TestBaseContextCase):
 
 	def open_web_browser(self, browser):
+
 		# Open web page
-		driver = Driver(browser)
-		self.page = AdminPageModel(driver=driver, implicit_wait_time=5, explicit_wait_time=10)
-		self.page.go()
+		with allure.step("Open web browser on: {}".format(AdminPageContext.URL)):
+			driver = Driver(browser)
+			self.page = AdminPageModel(driver=driver, implicit_wait_time=5, explicit_wait_time=10)
+			self.page.go()
 
-	def verify_page_url_title(self):
+	def verify_page_url(self):
 
-		self.assertEqual(AdminPageContext.URL,
-		                 self.page.url)
-		self.assertEqual(AdminPageContext.TITLE,
-		                 self.page.title)
+		with allure.step("Admin page URL test"):
+			self.assertEqual(AdminPageContext.URL,
+			                 self.page.url)
+
+	def verify_page_title(self):
+
+		with allure.step("Admin page title test"):
+			self.assertEqual(AdminPageContext.TITLE,
+			                 self.page.title)
 
 	def verify_parabank_admin_logo(self):
 
