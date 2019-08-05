68b46e2d20c26ccfc43050eac7137053b8d8700c
Egor Kostan <igorkostan@gmail.com>
2019-08-04 21:29:59 -0700

    Update home_page_context_case.py

--- tests/context_tests/home_page_context_case.py
+++ tests/context_tests/home_page_context_case.py
@@ -2,8 +2,8 @@ import allure
 from utils.driver import Driver
 from utils.screenshot import screenshot_on_fail
 from page_models.home_page_model import HomePageModel
-from expected_results.page_context.home_page_context import HomePageContext
 from tests.context_tests.base_context_case import TestBaseContextCase
+from expected_results.page_context.home_page_context import HomePageContext
 
 
 @allure.feature("Home Page")
@@ -19,38 +19,59 @@ class TestHomePageContextCase(TestBaseContextCase):
 			self.page = HomePageModel(driver=driver, implicit_wait_time=5, explicit_wait_time=10)
 			self.page.go()
 
+	@allure.feature("Base Page")
 	def verify_page_url(self):
 
 		with allure.step("Verify web page URL. Expected result: {}".format(HomePageContext.URL)):
 			self.assertEqual(HomePageContext.URL,
 			                 self.page.url)
 
+	@allure.feature("Base Page")
 	def verify_page_title(self):
 
 		with allure.step("Verify web page title. Expected result: {}".format(HomePageContext.TITLE)):
 			self.assertEqual(HomePageContext.TITLE,
 			                 self.page.title)
 
+	@allure.feature("Home Page")
 	def verify_atm_services_context(self):
 
-		self.assertEqual(HomePageContext.ATM_SERVICES['title'],
-		                 self.page.atm_title)
+		with allure.step("Verify ATM services title. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['title'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['title'],
+			                 self.page.atm_title)
 
-		self.assertEqual(HomePageContext.ATM_SERVICES['Withdraw Funds']['text'],
-		                 self.page.atm_withdraw_funds_text)
-		self.assertEqual(HomePageContext.ATM_SERVICES['Withdraw Funds']['href'],
-		                 self.page.atm_withdraw_funds_formated_href)
+		with allure.step("Verify ATM services > Withdraw Funds > text. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['Withdraw Funds']['text'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['Withdraw Funds']['text'],
+			                 self.page.atm_withdraw_funds_text)
 
-		self.assertEqual(HomePageContext.ATM_SERVICES['Check Balances']['text'],
-		                 self.page.atm_check_balances_text)
-		self.assertEqual(HomePageContext.ATM_SERVICES['Check Balances']['href'],
-		                 self.page.atm_check_balances_formated_href)
+		with allure.step("Verify ATM services > Withdraw Funds > href. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['Withdraw Funds']['href'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['Withdraw Funds']['href'],
+			                 self.page.atm_withdraw_funds_formated_href)
 
-		self.assertEqual(HomePageContext.ATM_SERVICES['Make Deposits']['text'],
-		                 self.page.atm_make_deposits_text)
-		self.assertEqual(HomePageContext.ATM_SERVICES['Make Deposits']['href'],
-		                 self.page.atm_make_deposits_formated_href)
+		with allure.step("Verify ATM services > Check Balances > text. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['Check Balances']['text'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['Check Balances']['text'],
+			                 self.page.atm_check_balances_text)
 
+		with allure.step("Verify ATM services > Check Balances > href. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['Check Balances']['href'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['Check Balances']['href'],
+			                 self.page.atm_check_balances_formated_href)
+
+		with allure.step("Verify ATM services > Make Deposits > text. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['Make Deposits']['text'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['Make Deposits']['text'],
+			                 self.page.atm_make_deposits_text)
+
+		with allure.step("Verify ATM services > Make Deposits > href. Expected result: {}".format(
+				HomePageContext.ATM_SERVICES['Make Deposits']['href'])):
+			self.assertEqual(HomePageContext.ATM_SERVICES['Make Deposits']['href'],
+			                 self.page.atm_make_deposits_formated_href)
+
+	@allure.feature("Home Page")
 	def verify_online_services_context(self):
 
 		self.assertEqual(HomePageContext.ONLINE_SERVICES['title'],
@@ -71,6 +92,7 @@ class TestHomePageContextCase(TestBaseContextCase):
 		self.assertEqual(HomePageContext.ONLINE_SERVICES['Transfer Funds']['href'],
 		                 self.page.online_transfer_funds_formated_href)
 
+	@allure.feature("Home Page")
 	def verify_read_more_services_button(self):
 
 		self.assertEqual(HomePageContext.READ_MORE_SERVICES['text'],
@@ -78,6 +100,7 @@ class TestHomePageContextCase(TestBaseContextCase):
 		self.assertEqual(HomePageContext.READ_MORE_SERVICES['href'],
 		                 self.page.read_more_services_formated_href)
 
+	@allure.feature("Home Page")
 	def verify_read_more_news_button(self):
 
 		self.assertEqual(HomePageContext.READ_MORE_NEWS['text'],
@@ -85,6 +108,7 @@ class TestHomePageContextCase(TestBaseContextCase):
 		self.assertEqual(HomePageContext.READ_MORE_NEWS['href'],
 		                 self.page.read_more_news_formated_href)
 
+	@allure.feature("Home Page")
 	def verify_latest_news_title(self):
 
 		self.assertEqual(HomePageContext.LATEST_NEWS,
