24e34f21804a0db562271af4d4898f639dd0b878
Egor Kostan <igorkostan@gmail.com>
2019-08-06 01:20:27 -0700

    Create base_personal_info_context.py

---
+++ expected_results/page_context/base_personal_info_context.py
@@ -0,0 +1,95 @@
+class BasePersonalInfoContext:
+	'''
+	Holds shared fields/data from "Forgot Login Info" and "Register" forms
+	'''
+
+	FORM = {
+		'first_name': {
+			'title': 'First Name:',
+			'input': {
+				'name': "customer.firstName",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'last_name': {
+			'title': 'Last Name:',
+			'input': {
+				'name': "customer.lastName",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'address': {
+			'title': 'Address:',
+			'input': {
+				'name': "customer.address.street",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'city': {
+			'title': 'City:',
+			'input': {
+				'name': "customer.address.city",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'state': {
+			'title': 'State:',
+			'input': {
+				'name': "customer.address.state",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'zip_code': {
+			'title': 'Zip Code:',
+			'input': {
+				'name': "customer.address.zipCode",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'phone': {
+			'title': 'Phone #:',
+			'input': {
+				'name': "customer.phoneNumber",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'ssn': {
+			'title': 'SSN:',
+			'input': {
+				'name': "customer.ssn",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'username': {
+			'title': 'Username:',
+			'input': {
+				'name': "customer.username",
+				'class': 'input',
+				'type': 'text'
+			}
+		},
+		'password': {
+			'title': 'Password:',
+			'input': {
+				'name': "customer.password",
+				'class': 'input',
+				'type': 'password'
+			}
+		},
+		'confirm': {
+			'title': 'Confirm:',
+			'input': {
+				'name': "repeatedPassword",
+				'class': 'input',
+				'type': 'password'
+			}
+		},
+	}
