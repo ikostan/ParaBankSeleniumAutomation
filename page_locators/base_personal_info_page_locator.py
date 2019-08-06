684680616168d56590c437c8fb5f7731c7c4da1d
Egor Kostan <igorkostan@gmail.com>
2019-08-05 17:45:01 -0700

    Create base_personal_info_page_locator.py

---
+++ page_locators/base_personal_info_page_locator.py
@@ -0,0 +1,34 @@
+from page_locators.base_page_locator import BasePageLocator
+from selenium.webdriver.common.by import By
+
+
+class BasePersonalInfoPageLocator:
+	'''
+	Holds all shared locators for 'Register' and 'Forgot Login Info' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
+
+	FIRST_NAME_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[1]/td[1]/b')
+	FIRST_NAME_INPUT = (By.XPATH, '//*[@id="customer.firstName"]')
+
+	LAST_NAME_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[2]/td[1]/b')
+	LAST_NAME_INPUT = (By.XPATH, '//*[@id="customer.lastName"]')
+
+	ADDRESS_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[3]/td[1]/b')
+	ADDRESS_INPUT = (By.XPATH, '//*[@id="customer.address.street"]')
+
+	CITY_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[4]/td[1]/b')
+	CITY_INPUT = (By.XPATH, '//*[@id="customer.address.city"]')
+
+	STATE_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[5]/td[1]/b')
+	STATE_INPUT = (By.XPATH, '//*[@id="customer.address.state"]')
+
+	ZIP_CODE_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[6]/td[1]/b')
+	ZIP_CODE_INPUT = (By.XPATH, '//*[@id="customer.address.zipCode"]')
+
+	PHONE_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[6]/td[1]/b')
+	PHONE_INPUT = (By.XPATH, '//*[@id="customer.address.zipCode"]')
+
+	SSN_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[6]/td[1]/b')
+	SSN_INPUT = (By.XPATH, '//*[@id="customer.address.zipCode"]')
