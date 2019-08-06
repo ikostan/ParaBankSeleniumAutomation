7ad7c0579412489da69357bf64e2291bd4fa8463
Egor Kostan <igorkostan@gmail.com>
2019-08-06 01:19:54 -0700

    Update chrome_services_page_context_test.py

--- tests/chrome/chrome_services_page_context_test.py
+++ tests/chrome/chrome_services_page_context_test.py
@@ -13,7 +13,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_page_url(self):
 		allure.dynamic.description("""
 		Context base elements validation > Services page URL:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do URL verification
 		""")
 		allure.dynamic.title("Web page URL test")
@@ -28,7 +28,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_page_title(self):
 		allure.dynamic.description("""
 		Context base elements validation > Services page title:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Services page title verification
 		""")
 		allure.dynamic.title("Services page title test")
@@ -43,7 +43,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_parabank_admin_logo(self):
 		allure.dynamic.description("""
 		Context base elements validation > Admin logo:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Admin logo verification
 		""")
 		allure.dynamic.title("Admin logo test")
@@ -73,7 +73,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_right_menu_home_button(self):
 		allure.dynamic.description("""
 		Context base elements validation > Right menu:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Right menu Home button verification
 		""")
 		allure.dynamic.title("Right menu Home button test")
@@ -88,7 +88,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_right_menu_contact_button(self):
 		allure.dynamic.description("""
 		Context base elements validation > Right menu:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Right menu Contact button verification
 		""")
 		allure.dynamic.title("Right menu Contact button test")
@@ -103,7 +103,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_right_menu(self):
 		allure.dynamic.description("""
 		Context base elements validation > Right menu:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Right menu About button verification
 		""")
 		allure.dynamic.title("Right menu About button test")
@@ -118,7 +118,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_solutions_menu_items(self):
 		allure.dynamic.description("""
 		Context base elements validation > Solution menu:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Solution verification
 		""")
 		allure.dynamic.title("Solution menu test")
@@ -133,7 +133,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_customer_login(self):
 		allure.dynamic.description("""
 		Context base elements validation > Customer Login:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Customer Login verification
 		""")
 		allure.dynamic.title("Customer Login test")
@@ -148,7 +148,7 @@ class TestChromeServicesPageContext(ServicesPageContextTestCase):
 	def test_footer_items(self):
 		allure.dynamic.description("""
 		Context base elements validation > Footer:
-			1. Open Services page web page
+			1. Open Services web page
 			2. Do Footer verification
 		""")
 		allure.dynamic.title("Footer test")
