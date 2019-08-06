3bb5df4705045c0854d0940d700a05fef2ad3cd6
Egor Kostan <igorkostan@gmail.com>
2019-08-05 17:56:21 -0700

    Create forgot_login_info_page_locator.py

---
+++ page_locators/forgot_login_info_page_locator.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+from page_locators.base_page_locator import BasePageLocator
+from page_locators.base_personal_info_page_locator import BasePersonalInfoPageLocator
+
+
+class ForgotLoginInfoPageLocator(BasePageLocator, BasePersonalInfoPageLocator):
+	'''
+	Holds all relevant locators for 'Services' page web elements.
+	Each locator is a tuple.
+	Separate the locator strings from the place where they are being used.
+	'''
+
+	FIND_MY_LOGIN_INFO_BUTTON = (By.XPATH,
+	                             '//*[@id="lookupForm"]/table/tbody/tr[8]/td[2]/'
+	                             'input[contains(@value, "Find My Login Info")])')
