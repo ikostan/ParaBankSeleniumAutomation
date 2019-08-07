293f1f2a2f6738b0d4047e227395f0cf2d1da9c3
Egor Kostan <igorkostan@gmail.com>
2019-08-07 00:09:24 -0700

    Update register_context_case.py

--- tests/context_tests/register_context_case.py
+++ tests/context_tests/register_context_case.py
@@ -7,7 +7,7 @@ from expected_results.page_context.register_page_context import RegisterPageCont
 from tests.context_tests.base_tests.base_personal_context_case import BasePersonalInfoContextCase
 
 
-@allure.feature("Register Page")
+# @allure.feature("Register Page")
 @allure.story('Register Context')
 @allure.suite('Register Page Context Test Suite')
 @screenshot_on_fail()
@@ -23,41 +23,42 @@ class RegisterContextCase(BasePersonalInfoContextCase):
 			self.page.go()
 			refresh_page(RegisterPageContext.TITLE, self.page)
 
-	@allure.feature("Register Page")
+	# Register Page Context
+	# @allure.feature("Register Page")
 	def verify_page_url(self):
 
 		with allure.step('Verify "Register" web page URL. Expected result: {}'.format(RegisterPageContext.URL)):
 			self.assertEqual(RegisterPageContext.URL,
 			                 self.page.url)
 
-	@allure.feature("Register Page")
+	# @allure.feature("Register Page")
 	def verify_page_title(self):
 
 		with allure.step('Verify "Register" web page title. Expected result: {}'.format(RegisterPageContext.TITLE)):
 			self.assertEqual(RegisterPageContext.TITLE,
 			                 self.page.title)
 
-	@allure.feature("Register Page")
+	# @allure.feature("Register Page")
 	def verify_username_title(self):
 
 		expected = RegisterPageContext.FORM['username']['title']
 		with allure.step('Verify "Username" title. Expected result: {}'.format(expected)):
 			self.assertEqual(expected, self.page.username_title)
 
-	@allure.feature("Register Page")
+	# @allure.feature("Register Page")
 	def verify_password_title(self):
 		expected = RegisterPageContext.FORM['password']['title']
 		with allure.step('Verify "Password" title. Expected result: {}'.format(expected)):
 			self.assertEqual(expected, self.page.password_title)
 
-	@allure.feature("Register Page")
+	# @allure.feature("Register Page")
 	def verify_confirm_title(self):
 
 		expected = RegisterPageContext.FORM['confirm']['title']
 		with allure.step('Verify "Confirm" title. Expected result: {}'.format(expected)):
 			self.assertEqual(expected, self.page.confirm_title)
 
-	@allure.feature("Register Page")
+	# @allure.feature("Register Page")
 	def verify_register_button(self):
 
 		with allure.step('Verify "Register" button label. Expected result: {}'.format(
@@ -72,3 +73,4 @@ class RegisterContextCase(BasePersonalInfoContextCase):
 				RegisterPageContext.REGISTER_BUTTON['type'])):
 			self.assertEqual(RegisterPageContext.REGISTER_BUTTON['type'], self.page.register_btn_type)
 
+	# "Base Personal Info Context"
