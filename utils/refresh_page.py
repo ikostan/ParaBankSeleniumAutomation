d7b11687c361e9c64e6c40d4980172372905b402
Egor Kostan <igorkostan@gmail.com>
2019-08-06 22:19:49 -0700

    Update refresh_page.py

--- utils/refresh_page.py
+++ utils/refresh_page.py
@@ -1,25 +1,26 @@
 import time
 
 
-def refresh_page(title, page):
+def refresh_page(expected, page, sleep_time=3):
 	'''
 	1. Check if page has expected title.
 	2. Trying to refresh web page in case page title dos not equal expected result
 	3. Raises exception in case page title dos not equal expected result
 
-	:param title:
+	:param expected:
+	:param sleep_time:
 	:param page:
 	:return:
 	'''
 
 	counter = 0
-	while title != page.title and counter < 5:
-		print("\nERROR: expected page title {} != {}.\nTrying to refresh...\n".format(title, page.title))
+	while expected != page.title and counter < 5:
+		print("\nERROR: expected page title {} != {}.\nTrying to refresh...\n".format(expected, page.title))
 		page.driver.refresh()
 		counter += 1
-		time.sleep(3)
+		time.sleep(sleep_time)
 
-	if title != page.title:
+	if expected != page.title:
 		raise Exception("\nERROR: This site can't be reached\n")
 
 	return None
