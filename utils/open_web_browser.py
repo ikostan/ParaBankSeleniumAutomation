ea3ea0f304e716607c7744110aba84f9542b6090
Egor Kostan <igorkostan@gmail.com>
2019-08-07 19:20:59 -0700

    Update open_web_browser.py

--- utils/open_web_browser.py
+++ utils/open_web_browser.py
@@ -6,7 +6,7 @@ from utils.refresh_page import refresh_page
 def open_web_browser(browser: str, page_model, page_context):
 
 	# Open web page
-	with allure.step("Open web browser on {}.".format(page_context.URL)):
+	with allure.step("Open following web page: {}.".format(page_context.URL)):
 		driver = Driver(browser)
 		page = page_model(driver=driver, implicit_wait_time=5, explicit_wait_time=10)
 		page.go()
