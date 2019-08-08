2da01ac289fb0763bebf6140f06ead4d8bc1a607
Egor Kostan <igorkostan@gmail.com>
2019-08-08 00:27:49 -0700

    Update services_page_context_case.py

--- tests/context_tests/services_page_context_case.py
+++ tests/context_tests/services_page_context_case.py
@@ -4,26 +4,11 @@ from tests.context_tests.base_cases.base_context_case import BaseContextCase
 from expected_results.page_context.services_page_context import ServicesPageContext
 
 
-@allure.feature("Services Page")
 @allure.story('Services Context')
 @allure.suite('Services Page Context Test Suite')
 @screenshot_on_fail()
 class ServicesPageContextCase(BaseContextCase):
 
-	'''
-	@staticmethod
-	def open_web_browser(browser):
-
-		# with allure.step('Open web browser on: {}'.format(ServicesPageContext.URL)):
-		# Open web page
-		driver = Driver(browser)
-		page = ServicesPageModel(driver=driver, implicit_wait_time=5, explicit_wait_time=10)
-		page.go()
-		refresh_page(ServicesPageContext.TITLE, page)
-		return page
-	'''
-
-	# @allure.feature("Services Page")
 	def verify_page_url(self):
 
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
@@ -31,7 +16,6 @@ class ServicesPageContextCase(BaseContextCase):
 			self.assertEqual(ServicesPageContext.URL,
 			                 self.page.url)
 
-	# @allure.feature("Services Page")
 	def verify_page_title(self):
 		allure.dynamic.severity(allure.severity_level.MINOR)
 		with allure.step('Verify "Services" web page title. Expected result: {}'.format(ServicesPageContext.TITLE)):
