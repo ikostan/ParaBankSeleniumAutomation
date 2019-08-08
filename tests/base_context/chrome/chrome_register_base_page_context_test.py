a66282cca59f2ce3eeb1bc35cb36400e661945e1
Egor Kostan <igorkostan@gmail.com>
2019-08-08 02:15:29 -0700

    Update chrome_register_base_page_context_test.py

--- tests/base_context/chrome/chrome_register_base_page_context_test.py
+++ tests/base_context/chrome/chrome_register_base_page_context_test.py
@@ -1,6 +1,7 @@
 import allure
 from utils.screenshot import screenshot_on_fail
 from utils.open_web_browser import open_web_browser
+from utils.http_status_code import get_http_status_code
 from page_models.register_page_model import RegisterPageModel
 from tests.context_tests.register_page_context_case import RegisterContextCase
 from expected_results.page_context.register_page_context import RegisterPageContext
@@ -18,6 +19,7 @@ class TestChromeRegisterBasePageContext(RegisterContextCase):
 			cls.browser = 'chrome'
 			cls.page_model = RegisterPageModel
 			cls.page_context = RegisterPageContext
+			get_http_status_code(RegisterPageContext.URL)
 			cls.page = open_web_browser(browser=cls.browser,
 			                            page_model=cls.page_model,
 			                            page_context=cls.page_context)
