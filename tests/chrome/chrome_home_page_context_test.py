3f05cc24971f79cc65474647505a0aba5fcc5a60
Egor Kostan <igorkostan@gmail.com>
2019-08-07 19:21:55 -0700

    Update chrome_home_page_context_test.py

--- tests/chrome/chrome_home_page_context_test.py
+++ tests/chrome/chrome_home_page_context_test.py
@@ -76,6 +76,7 @@ class TestChromeHomePageContext(HomePageContextCase):
 		# self.open_web_browser()
 
 		# Verify ATM Services Context
+		# allure.step
 		self.verify_atm_services_context()
 
 	@allure.feature("Home Page")
@@ -117,11 +118,10 @@ class TestChromeHomePageContext(HomePageContextCase):
 		""")
 		allure.dynamic.title("'Read More' (news) button test")
 
-		# open web browser
-		# self.open_web_browser()
-
 		# Verify Read More News button
-		self.verify_read_more_news_button()
+		with allure.step("Verify Read More News button attributes:"):
+			self.verify_read_more_news_button_text()
+			self.verify_read_more_news_button_href()
 
 	@allure.feature("Home Page")
 	def test_latest_news_title(self):
@@ -137,129 +137,3 @@ class TestChromeHomePageContext(HomePageContextCase):
 
 		# Verify Last News title
 		self.verify_latest_news_title()
-
-	@allure.feature("Base Page")
-	def test_parabank_admin_logo(self):
-		allure.dynamic.description("""
-		Context base elements validation > Admin logo:
-			1. Open Home web page
-			2. Do Admin logo verification
-		""")
-		allure.dynamic.title("Admin logo test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_parabank_admin_logo()
-
-	@allure.feature("Base Page")
-	def test_parabank_logo(self):
-		allure.dynamic.description("""
-		Context base elements validation > 'ParaBank' logo:
-			1. Open Home web page
-			2. Do Admin logo verification
-		""")
-		allure.dynamic.title("'ParaBank' logo test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_parabank_logo()
-
-	@allure.feature("Base Page")
-	def test_right_menu_home_button(self):
-		allure.dynamic.description("""
-		Context base elements validation > 'Home' button:
-			1. Open Home web page
-			2. Do 'Home' button (right menu) verification
-		""")
-		allure.dynamic.title("'Home' button (right menu) test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_right_menu_home_button()
-
-	@allure.feature("Base Page")
-	def test_right_menu_about_button(self):
-		"""Context base elements validation > Home button:
-		1. Open Home web page
-		2. Do About button verification: href + text
-		"""
-		allure.dynamic.description("""
-		Context base elements validation > 'About' button:
-			1. Open Home web page
-			2. Do 'About' button (right menu) verification
-		""")
-		allure.dynamic.title("'About' button (right menu) test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_right_menu_about_button()
-
-	@allure.feature("Base Page")
-	def test_right_menu_contact_button(self):
-		allure.dynamic.description("""
-		Context base elements validation > 'Contact' button:
-			1. Open Home web page
-			2. Do 'Contact' button (right menu) verification
-		""")
-		allure.dynamic.title("'Contact' button (right menu) test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_right_menu_contact_button()
-
-	@allure.feature("Base Page")
-	def test_solutions_menu_items(self):
-		allure.dynamic.description("""
-		Context base elements validation > 'Solutions' menu items:
-			1. Open Home web page
-			2. Do 'Solutions' menu items verification
-		""")
-		allure.dynamic.title("'Solutions' menu items test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_solutions_menu_items()
-
-	@allure.feature("Base Page")
-	def test_customer_login(self):
-		allure.dynamic.description("""
-		Context base elements validation > 'Customer Login':
-			1. Open Home web page
-			2. Do 'Customer Login' items verification
-		""")
-		allure.dynamic.title("'Customer Login' test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_customer_login()
-
-	@allure.feature("Base Page")
-	def test_footer_items(self):
-		allure.dynamic.description("""
-		Context base elements validation > 'Footer':
-			1. Open Home web page
-			2. Do 'Footer' items verification
-		""")
-
-		allure.dynamic.title("'Footer' test")
-
-		# open web browser
-		# self.open_web_browser()
-
-		# Context base elements validation:
-		self.verify_footer_items()
-
