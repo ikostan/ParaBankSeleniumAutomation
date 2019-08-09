6749db6847ad7177c3817d681c04ddd99d0c8159
Egor Kostan <igorkostan@gmail.com>
2019-08-09 16:01:35 -0700

    Update register_user.py

--- utils/register_user.py
+++ utils/register_user.py
@@ -1,4 +1,10 @@
-def register_user(page, client):
+from utils.http_status_code import get_http_status_code
+from utils.open_web_browser import open_web_browser
+from page_models.register_page_model import RegisterPageModel
+from expected_results.page_context.register_page_context import RegisterPageContext
+
+
+def register_user(client):
 	'''
 	Register a new user.
 	Does not check for any errors.
@@ -7,6 +13,17 @@ def register_user(page, client):
 	:return:
 	'''
 
+	page_model = RegisterPageModel
+	page_context = RegisterPageContext
+	browser = 'chrome'
+
+	print("Open web browser")
+	get_http_status_code(RegisterPageContext.URL)
+	page = open_web_browser(browser=browser,
+	                        page_model=page_model,
+	                        page_context=page_context)
+
+	print("Filling out user data...")
 	page.type_first_name(client.FIRST_NAME)
 
 	page.type_last_name(client.LAST_NAME)
@@ -29,4 +46,8 @@ def register_user(page, client):
 
 	page.type_confirm(client.PASSWORD)
 
+	print("Clicking on Register button")
 	page.click_register_btn()
+
+	print("\nClose web browser")
+	page.quit()
