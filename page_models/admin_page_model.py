d58c53fe0d63f7de9e05d7f69c8f774324a2c185
Egor Kostan <igorkostan@gmail.com>
2019-08-09 14:51:17 -0700

    Update admin_page_model.py

--- page_models/admin_page_model.py
+++ page_models/admin_page_model.py
@@ -1,4 +1,6 @@
+from elements.element import Element
 from page_models.base_page_model import BasePageModel
+from page_locators.admin_page_locator import AdminPageLocator
 from expected_results.page_context.admin_page_context import AdminPageContext
 
 
@@ -10,4 +12,20 @@ class AdminPageModel(BasePageModel):
 
 	_url = AdminPageContext.URL
 
+	def hit_initialize_button(self):
+		'''
+		Hit Initialize button
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, AdminPageLocator.INITIALIZE_BUTTON)
+		element.press_button()
+		return None
 
+	def hit_clean_button(self):
+		'''
+		Hit Clean button
+		:return:
+		'''
+		element = Element(self.driver, self.explicit_wait_time, AdminPageLocator.CLEAN_BUTTON)
+		element.press_button()
+		return None
