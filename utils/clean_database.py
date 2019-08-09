2ce69b035332b4c7e75a964f86407d159f9c9719
Egor Kostan <igorkostan@gmail.com>
2019-08-09 16:01:39 -0700

    Update clean_database.py

--- utils/clean_database.py
+++ utils/clean_database.py
@@ -1,15 +1,34 @@
 import time
+from utils.http_status_code import get_http_status_code
+from utils.open_web_browser import open_web_browser
+from page_models.admin_page_model import AdminPageModel
+from expected_results.page_context.admin_page_context import AdminPageContext
 
 
-def clean_database(page):
+def clean_database():
 	'''
 	Clean and initialise database.
 	Does not check for any errors.
 	:return:
 	'''
 
-	page.hit_clean_button()
+	page_model = AdminPageModel
+	page_context = AdminPageContext
+	browser = 'chrome'
 
-	time.sleep(3)
-	
+	print("Open web browser")
+	get_http_status_code(AdminPageContext.URL)
+	page = open_web_browser(browser=browser,
+	                        page_model=page_model,
+	                        page_context=page_context)
+
+	print("\nCleaning database...")
+	page.hit_clean_button()
+	time.sleep(2)
+
+	print("\nInitializing database...")
 	page.hit_initialize_button()
+	time.sleep(2)
+
+	print("\nClose web browser")
+	page.quit()
