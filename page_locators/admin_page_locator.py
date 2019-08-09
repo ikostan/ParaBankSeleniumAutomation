6dff96f60addb98d1b8f80609dc85d91406158ea
Egor Kostan <igorkostan@gmail.com>
2019-08-09 14:51:20 -0700

    Update admin_page_locator.py

--- page_locators/admin_page_locator.py
+++ page_locators/admin_page_locator.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.common.by import By
 from page_locators.base_page_locator import BasePageLocator
 
 
@@ -7,4 +8,7 @@ class AdminPageLocator(BasePageLocator):
 	Each locator is a tuple.
 	Separate the locator strings from the place where they are being used.
 	'''
-	pass
+
+	INITIALIZE_BUTTON = (By.XPATH, '//button[contains(text(),\'Initialize\')]')
+
+	CLEAN_BUTTON = (By.XPATH, '//button[contains(text(),\'Clean\')]')
