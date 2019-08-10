d508af1e9227329678eb6e6d14a84f67f02d53dc
Egor Kostan <igorkostan@gmail.com>
2019-08-10 16:17:07 -0700

    Update open_web_browser.py

--- utils/open_web_browser.py
+++ utils/open_web_browser.py
@@ -9,6 +9,19 @@ from utils.http_status_code import get_http_status_code
 
 
 def open_web_browser(browser: str, page_model, page_context):
+	'''
+	Take care of following procedure:
+		1. Instantiate Selenium webdriver
+		2. Instantiate Page Model Object
+		3. Checks HTTP status code
+		4. Open web browser > open test web page
+		5. Refresh web browser in case test web site is not loaded
+		6. Returns page instance (Page Model Object)
+	:param browser:
+	:param page_model:
+	:param page_context:
+	:return:
+	'''
 
 	# Open web page
 	with allure.step("Open following web page: {}.".format(page_context.URL)):
