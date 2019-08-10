4385489cf367bdb7eec16ea23a3610ba17e798f5
Egor Kostan <igorkostan@gmail.com>
2019-08-09 21:33:57 -0700

    Create browser_configuration.py

---
+++ utils/browser_configuration.py
@@ -0,0 +1,16 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+def browser_configuration():
+	'''
+	Returns browser name from dictionary
+	:return:
+	'''
+
+	browsers = {1: 'chrome',
+	            2: 'edge',
+	            3: 'mozilla'}
+
+	return browsers[1]
