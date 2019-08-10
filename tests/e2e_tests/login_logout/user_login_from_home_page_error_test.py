bb3fd6716667cef518d319157fbc911d318c59a3
Egor Kostan <igorkostan@gmail.com>
2019-08-10 01:34:44 -0700

    Update user_login_from_home_page_error_test.py

--- tests/e2e_tests/login_logout/user_login_from_home_page_error_test.py
+++ tests/e2e_tests/login_logout/user_login_from_home_page_error_test.py
@@ -15,11 +15,14 @@ from expected_results.page_context.home_page_context import HomePageContext
 from tests.context_tests.context_cases.home_page_context_case import HomePageContextCase
 
 
-@allure.suite("End To End")
-@allure.sub_suite('Home Page')
-@allure.feature("User Login/Logout")
+@allure.epic('Page Functionality')
+@allure.parent_suite('End To End')
+@allure.suite("User Login/Logout")
+@allure.sub_suite('Positive Tests')
+@allure.feature("Home Page")
+@allure.story('Login/Logout Functionality')
 @screenshot_on_fail()
-class TestUserLoginFromHomePage(HomePageContextCase):
+class TestUserLoginFromHomePageError(HomePageContextCase):
 
 	@classmethod
 	def setUpClass(cls):
