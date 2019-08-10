87b84d4d002d2d436fb4bcf7ace98cb8f73f10f7
Egor Kostan <igorkostan@gmail.com>
2019-08-09 21:51:32 -0700

    Update services_page_context_case.py

--- tests/context_tests/context_cases/services_page_context_case.py
+++ tests/context_tests/context_cases/services_page_context_case.py
@@ -3,7 +3,10 @@
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 import allure
+
 from utils.screenshot import screenshot_on_fail
+from utils.step_definition import step_definition
+
 from expected_results.page_context.services_page_context import ServicesPageContext
 from tests.context_tests.context_cases.base_cases.base_context_case import BaseContextCase
 
@@ -15,9 +18,19 @@ class ServicesPageContextCase(BaseContextCase):
 
 	def verify_page_url(self):
 
+		expected = ServicesPageContext.URL
+		actual = self.page.url
+		step_description = 'Verify "Services" web page URL. Expected result: {}'.format(ServicesPageContext.URL)
+		severity = allure.severity_level.BLOCKER
+
+		step_definition(self, expected, actual, step_description, severity)
+	'''
+	def verify_page_url(self):
+
 		allure.dynamic.severity(allure.severity_level.BLOCKER)
 		with allure.step('Verify "Services" web page URL. Expected result: {}'.format(ServicesPageContext.URL)):
 			self.assertEqual(ServicesPageContext.URL, self.page.url)
+	'''
 
 	def verify_page_title(self):
 		allure.dynamic.severity(allure.severity_level.MINOR)
