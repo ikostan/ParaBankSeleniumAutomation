c6fca073d2ae05e3a7685ce5955b1bcf310f2c4c
Egor Kostan <igorkostan@gmail.com>
2019-08-09 21:58:42 -0700

    Update admin_base_page_context_test.py

--- tests/context_tests/base_context_tests/admin_base_page_context_test.py
+++ tests/context_tests/base_context_tests/admin_base_page_context_test.py
@@ -2,13 +2,19 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
 import allure
+
 from utils.screenshot import screenshot_on_fail
 from utils.open_web_browser import open_web_browser
+from utils.browser_configuration import browser_configuration
+
 from page_models.admin_page_model import AdminPageModel
-from utils.http_status_code import get_http_status_code
-from tests.context_tests.context_cases import AdminPageContextCase
 from expected_results.page_context.admin_page_context import AdminPageContext
+from tests.context_tests.context_cases.admin_page_context_case import AdminPageContextCase
 
 
 @allure.suite("Base Context Testing")
@@ -20,10 +26,9 @@ class TestChromeAdminBasePageContext(AdminPageContextCase):
 	@classmethod
 	def setUpClass(cls):
 		with allure.step("Open web browser"):
-			cls.browser = 'chrome'
+			cls.browser = browser_configuration()
 			cls.page_model = AdminPageModel
 			cls.page_context = AdminPageContext
-			get_http_status_code(AdminPageContext.URL)
 			cls.page = open_web_browser(browser=cls.browser,
 			                            page_model=cls.page_model,
 			                            page_context=cls.page_context)
