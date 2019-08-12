3a4a9fd32869a9d2dfb90c830c4ead11e3453897
Egor Kostan <igorkostan@gmail.com>
2019-08-12 13:07:55 -0700

    Delete customer_lookup_page_context.py

--- expected_results/page_context/customer_lookup_page_context.py
+++
@@ -1,9 +0,0 @@
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
-
-from expected_results.page_context.base_page_context import BasePageContext
-
-
-class CustomerLookupPageContext(BasePageContext):
-	pass
