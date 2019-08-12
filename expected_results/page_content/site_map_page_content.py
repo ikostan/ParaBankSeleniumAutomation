06ef5009dfe9fac92fcda94f9d3b06d9b540bcb3
Egor Kostan <igorkostan@gmail.com>
2019-08-12 13:08:22 -0700

    Create site_map_page_content.py

---
+++ expected_results/page_content/site_map_page_content.py
@@ -0,0 +1,10 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+from expected_results.page_content.base_page_content import BasePageContent
+
+
+class SiteMapPageContent(BasePageContent):
+	pass
