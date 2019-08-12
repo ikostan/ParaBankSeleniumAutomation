96e5a016151e4c1528c14b6e08e604873d9a0026
Egor Kostan <igorkostan@gmail.com>
2019-08-12 13:08:42 -0700

    Create bank_account_content.py

---
+++ expected_results/page_content/bank_account_content.py
@@ -0,0 +1,32 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+from expected_results.page_content.base_page_content import BasePageContent
+
+
+class BankAccountContent(BasePageContent):
+
+	ACCOUNT_SERVICES_MENU = {
+		'welcome_message': "Welcome ",
+		'title': 'Account Services',
+		"menu_items": {
+			'open_new_account': {'text': 'Open New Account',
+			                     "href": "/parabank/openaccount.htm"},
+			'account_overview': {'text': 'Accounts Overview',
+			                     "href": "/parabank/overview.htm"},
+			'transfer_funds': {'text': 'Transfer Funds',
+			                   "href": "/parabank/transfer.htm"},
+			'bill_pay': {'text': 'Bill Pay',
+			             "href": "/parabank/billpay.htm"},
+			'find_transactions': {'text': 'Find Transactions',
+			                      "href": "/parabank/findtrans.htm"},
+			'update_info': {'text': 'Update Contact Info',
+			                "href": "/parabank/updateprofile.htm"},
+			'request_loan': {'text': 'Request Loan',
+			                 "href": "/parabank/requestloan.htm"},
+			'log_out': {'text': 'Log Out',
+			            "href": "/parabank/logout.htm"}
+		}
+	}
+
