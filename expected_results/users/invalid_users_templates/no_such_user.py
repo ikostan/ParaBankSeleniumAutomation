c511dcf6b1af05c2c8a5e095332c53191b05de47
Egor Kostan <igorkostan@gmail.com>
2019-08-13 11:51:41 -0700

    Create no_such_user.py

---
+++ expected_results/users/invalid_users_templates/no_such_user.py
@@ -0,0 +1,25 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class NoSuchUser:
+
+	FIRST_NAME = ''
+	LAST_NAME = ''
+
+	ADDRESS = ''
+	CITY = ''
+	STATE = ''
+	ZIP_CODE = ''
+
+	PHONE = ''
+
+	USERNAME = 'noname'
+	PASSWORD = 'nopassword'
+
+	SSN = ''
