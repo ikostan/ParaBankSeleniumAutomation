618194938b3168ecbfae690d572732cb83b69357
Egor Kostan <igorkostan@gmail.com>
2019-08-13 13:30:15 -0700

    Update register_page_locator.py

--- page_locators/register_page_locator.py
+++ page_locators/register_page_locator.py
@@ -16,11 +16,14 @@ class RegisterPageLocator(BasePageLocator, BasePersonalInfoPageLocator):
 
 	USERNAME_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[10]/td[1]/b')
 	USERNAME_INPUT = (By.XPATH, '//*[@id="customer.username"]')
+	USERNAME_ERROR = (By.XPATH, '//*[@id="customer.username.errors"]')
 
 	PASSWORD_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[11]/td[1]/b')
 	PASSWORD_INPUT = (By.XPATH, '//*[@id="customer.password"]')
+	PASSWORD_ERROR = (By.XPATH, '//*[@id="customer.password.errors"]')
 
 	CONFIRM_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[12]/td[1]/b')
 	CONFIRM_INPUT = (By.XPATH, '//*[@id="repeatedPassword"]')
+	CONFIRM_ERROR = (By.XPATH, '//*[@id="repeatedPassword.errors"]')
 
 	REGISTER_BUTTON = (By.XPATH, '//input[contains(@value, "Register")]')
