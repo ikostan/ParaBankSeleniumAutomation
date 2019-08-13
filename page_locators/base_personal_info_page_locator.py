66af16b8cd19018064803e4b7678e83ee272ea0d
Egor Kostan <igorkostan@gmail.com>
2019-08-13 13:29:59 -0700

    Update base_personal_info_page_locator.py

--- page_locators/base_personal_info_page_locator.py
+++ page_locators/base_personal_info_page_locator.py
@@ -17,24 +17,31 @@ class BasePersonalInfoPageLocator:
 
 	FIRST_NAME_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[1]/td[1]/b')
 	FIRST_NAME_INPUT = (By.XPATH, '//*[@id="customer.firstName"]')
+	FIRST_NAME_ERROR = (By.XPATH, '//*[@id="customer.firstName.errors"]')
 
 	LAST_NAME_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[2]/td[1]/b')
 	LAST_NAME_INPUT = (By.XPATH, '//*[@id="customer.lastName"]')
+	LAST_NAME_ERROR = (By.XPATH, '//*[@id="customer.lastName.errors"]')
 
 	ADDRESS_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[3]/td[1]/b')
 	ADDRESS_INPUT = (By.XPATH, '//*[@id="customer.address.street"]')
+	ADDRESS_ERROR = (By.XPATH, '//*[@id="customer.address.street.errors"]')
 
 	CITY_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[4]/td[1]/b')
 	CITY_INPUT = (By.XPATH, '//*[@id="customer.address.city"]')
+	CITY_ERROR = (By.XPATH, '//*[@id="customer.address.city.errors"]')
 
 	STATE_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[5]/td[1]/b')
 	STATE_INPUT = (By.XPATH, '//*[@id="customer.address.state"]')
+	STATE_ERROR = (By.XPATH, '//*[@id="customer.address.state.errors"]')
 
 	ZIP_CODE_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[6]/td[1]/b')
 	ZIP_CODE_INPUT = (By.XPATH, '//*[@id="customer.address.zipCode"]')
+	ZIP_CODE_ERROR = (By.XPATH, '//*[@id="customer.address.zipCode.errors"]')
 
 	PHONE_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[7]/td[1]/b')
 	PHONE_INPUT = (By.XPATH, '//*[@id="customer.phoneNumber"]')
 
 	SSN_TITLE = (By.XPATH, '//*[@id="customerForm"]/table/tbody/tr[8]/td[1]/b')
 	SSN_INPUT = (By.XPATH, '//*[@id="customer.ssn"]')
+	SSN_ERROR = (By.XPATH, '//*[@id="customer.ssn.errors"]')
