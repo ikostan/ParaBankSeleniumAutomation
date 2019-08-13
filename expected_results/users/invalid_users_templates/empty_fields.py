c43694362399547dcf0db57ec10a2b71a1502f9b
Egor Kostan <igorkostan@gmail.com>
2019-08-13 11:51:38 -0700

    Create empty_fields.py

---
+++ expected_results/users/invalid_users_templates/empty_fields.py
@@ -0,0 +1,33 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class EmptyFields:
+	'''
+	Fake Person Generator:
+	Source: https://www.fakepersongenerator.com/random-los-angeles-address-generator
+	'''
+
+	FIRST_NAME = ''
+	LAST_NAME = ''
+
+	ADDRESS = ''
+	CITY = ''
+	STATE = ''
+	ZIP_CODE = ''
+
+	PHONE = ''
+
+	USERNAME = ''
+	PASSWORD = USERNAME + ZIP_CODE
+
+	SSN = ''
+
+	INIT_BALANCE = 0.00
+	MIN_BALANCE = 0.00
+
