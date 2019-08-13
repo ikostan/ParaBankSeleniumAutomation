6933c5c28aa8264760f7bf81f86fb6bcbfdcaaa5
Egor Kostan <igorkostan@gmail.com>
2019-08-13 13:30:22 -0700

    Update user_registration_test.py

--- tests/e2e_tests/user_registration/user_registration_test.py
+++ tests/e2e_tests/user_registration/user_registration_test.py
@@ -4,16 +4,21 @@
 
 import allure
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from utils.screenshot import screenshot_on_fail
 from utils.clean_database import clean_database
 from utils.open_web_browser import open_web_browser
 
 from expected_results.users.base_user import BaseUser
+from expected_results.page_content.home_page_content import HomePageContent
 from expected_results.users.valid_users_templates.jane_doe import JaneDoe
-from utils.http_status_code import get_http_status_code
+
+from page_locators.register_page_locator import RegisterPageLocator
 from page_object_models.register_page_model import RegisterPageModel
 from expected_results.page_content.register_page_content import RegisterPageContent
-from tests.e2e_tests.user_registration.base_case.register_new_user import UserRegistrationCase
+from tests.e2e_tests.user_registration.base_case.user_registration_case import UserRegistrationCase
 
 
 @allure.epic('Page Functionality')
@@ -46,7 +51,6 @@ class TestUserRegistration(UserRegistrationCase):
 			self.page_model = RegisterPageModel
 			self.page_context = RegisterPageContent
 			with allure.step("Open web browser"):
-				get_http_status_code(RegisterPageContent.URL)
 				self.page = open_web_browser(browser=self.browser,
 				                             page_model=self.page_model,
 				                             page_content=self.page_context)
@@ -73,5 +77,76 @@ class TestUserRegistration(UserRegistrationCase):
 		allure.dynamic.severity(allure.severity_level.CRITICAL)
 
 		# Register a new user:
-		self.register_new_user()
+		self.fill_out_user_data()
+
+		with allure.step('Hit "Register" button'):
+			print('Hit "Register" button')
+			self.page.click_register_btn()
+
+		with allure.step('Wait for "Welcome" message'):
+			WebDriverWait(self.page.driver,
+			              self.page.explicit_wait_time).until(
+				EC.presence_of_element_located(RegisterPageLocator.HEADER))
+
+		with allure.step('Verify "Welcome" header'):
+			expected = RegisterPageContent.WELCOME_MESSAGE['header'] + self.client.username
+			actual = self.page.welcome_header
+			print('\nStep: {}\nExpected: {}\nActual: {}'.format('\'Verify "Welcome" header\'',
+			                                                    expected,
+			                                                    actual))
+			self.assertEqual(expected,
+			                 actual,
+			                 msg="Expected <{}> value does not equal actual <{}> result".format(expected,
+			                                                                                    actual))
+
+		with allure.step('Verify "Welcome" message'):
+			expected = RegisterPageContent.WELCOME_MESSAGE['message']
+			actual = self.page.welcome_message
+			print('\nStep: {}\nExpected: {}\nActual: {}'.format('\'Verify "Welcome" header\'',
+			                                                    expected,
+			                                                    actual))
+			self.assertEqual(expected,
+			                 actual,
+			                 msg="Expected <{}> value does not equal actual <{}> result".format(expected,
+			                                                                                    actual))
+
+		with allure.step('Verify that "Account Services" menu is present'):
+			expected = True
+			actual = self.page.account_services_menu_is_visible
+			print('\nStep: {}\nExpected: {}\nActual: {}'.format('"Account Services" menu is present',
+			                                                    expected,
+			                                                    actual))
+			self.assertEqual(expected,
+			                 actual,
+			                 msg="Expected <{}> value does not equal actual <{}> result".format(expected,
+			                                                                                    actual))
+		# Logout
+		with allure.step('Hit "Log Out" link'):
+			print('Hit "Log Out" link')
+			self.page.log_out()
+			WebDriverWait(self.page.driver, self.page.explicit_wait_time).until(EC.url_contains('index.htm'))
+
+		# Post Logout validation
+		with allure.step('Verify URL'):
+			expected = HomePageContent.URL
+			actual = self.page.url
+			print('\nStep: {}\nExpected: {}\nActual: {}'.format('\'Verify URL\'',
+			                                                    expected,
+			                                                    actual))
+			self.assertEqual(expected,
+			                 actual,
+			                 msg="Expected <{}> value does not equal actual <{}> result".format(expected,
+			                                                                                    actual))
+
+		with allure.step('Verify that "Account Services" menu is not present'):
+			expected = False
+			actual = self.page.account_services_menu_is_visible
+			print('\nStep: {}\nExpected: {}\nActual: {}'.format('"Account Services" menu is not present',
+			                                                    expected,
+			                                                    actual))
+			self.assertEqual(expected,
+			                 actual,
+			                 msg="Expected <{}> value does not equal actual <{}> result".format(expected,
+			                                                                                    actual))
+
 
