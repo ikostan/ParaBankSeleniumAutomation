82d2256561479a584cc61fcd8d461ef8ef5d3573
Egor Kostan <igorkostan@gmail.com>
2019-08-13 12:52:24 -0700

    Update services_base_page_content_test.py

--- tests/content_tests/base_content_tests/services_base_page_content_test.py
+++ tests/content_tests/base_content_tests/services_base_page_content_test.py
@@ -10,7 +10,7 @@ from utils.browser_configuration import browser_configuration
 
 from page_object_models.services_page_model import ServicesPageModel
 from expected_results.page_content.services_page_content import ServicesPageContent
-from tests.content_tests.content_cases.base_cases.base_content_case import BaseContentCase
+from tests.content_tests.base_cases.base_content_case import BaseContentCase
 
 
 @allure.epic('Page Content')
