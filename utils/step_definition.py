f072b5cb366e3144bf62dd8ab8838c3fbd921f11
Egor Kostan <igorkostan@gmail.com>
2019-08-14 15:17:49 -0700

    Update step_definition.py

--- utils/step_definition.py
+++ utils/step_definition.py
@@ -5,13 +5,33 @@
 import allure
 
 
-def step_definition(self, expected, actual, step_description='N/A'):
+def step_definition(self, expected, actual, act=None, step_description='N/A'):
+	"""
+	Unit Testing and the Arrange, Act and Assert (AAA) Pattern
+	:param self:
+	:param expected:
+	:param act:
+	:param actual:
+	:param step_description:
+	:return:
+	"""
 
 	with allure.step(step_description):
+		# Arrange
+		expected = expected
+
+		# Act
+		if act:
+			act(expected)
+
+		# Assert
+		actual_result = actual()
+
+		# Log
 		print('\nStep: {}\nExpected: {}\nActual: {}'.format(step_description,
 		                                                    expected,
-		                                                    actual))
+		                                                    actual_result))
 		self.assertEqual(expected,
-		                 actual,
+		                 actual_result,
 		                 msg="Expected <{}> value does not equal actual <{}> result".format(expected,
-		                                                                                    actual))
+		                                                                                    actual_result))
