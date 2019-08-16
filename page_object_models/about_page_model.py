e1b037cd91f8d1f156ae47905f8f42e1394c1389
Egor Kostan <igorkostan@gmail.com>
2019-08-15 22:11:51 -0700

    Update about_page_model.py

--- page_object_models/about_page_model.py
+++ page_object_models/about_page_model.py
@@ -2,7 +2,7 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-from elements.element import Element
+from element_object_models.element import Element
 from page_object_models.base_page_model import BasePageModel
 from page_locators.about_page_locator import AboutPageLocator
 from expected_results.page_content.about_page_content import AboutPageContent
