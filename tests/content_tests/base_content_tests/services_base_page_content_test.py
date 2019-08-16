98b9c48f3d61e36c5bff480062814caddce93ec9
Egor Kostan <igorkostan@gmail.com>
2019-08-15 18:18:24 -0700

    Update services_base_page_content_test.py

--- tests/content_tests/base_content_tests/services_base_page_content_test.py
+++ tests/content_tests/base_content_tests/services_base_page_content_test.py
@@ -4,9 +4,9 @@
 
 import allure
 
+from tests.config import Config
 from utils.screenshot import screenshot_on_fail
 from utils.open_web_browser import open_web_browser
-from utils.browser_configuration import browser_configuration
 
 from page_object_models.services_page_model import ServicesPageModel
 from expected_results.page_content.services_page_content import ServicesPageContent
@@ -25,10 +25,11 @@ class TestServicesBasePageContent(BaseContentCase):
 	@classmethod
 	def setUpClass(cls):
 		with allure.step("Open web browser"):
-			cls.browser = browser_configuration()
+			# cls.browser = browser_configuration()
+			cls.app_config = Config()
 			cls.page_model = ServicesPageModel
 			cls.page_content = ServicesPageContent
-			cls.page = open_web_browser(browser=cls.browser,
+			cls.page = open_web_browser(browser=cls.app_config.browser,
 			                            page_model=cls.page_model,
 			                            page_content=cls.page_content)
 
