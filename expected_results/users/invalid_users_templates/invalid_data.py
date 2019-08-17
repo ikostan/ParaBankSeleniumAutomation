98a3c285592926a7a5160ad562ffc133c3becbff
Egor Kostan <igorkostan@gmail.com>
2019-08-17 08:31:21 -0700

    Create invalid_data.py

---
+++ expected_results/users/invalid_users_templates/invalid_data.py
@@ -0,0 +1,29 @@
+#  Created by Egor Kostan.
+#  GitHub: https://github.com/ikostan
+#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
+
+
+class InvalidData:
+	'''
+	Fake Person Generator:
+	Source: https://www.fakepersongenerator.com/random-los-angeles-address-generator
+	'''
+
+	FIRST_NAME = '31234'
+	LAST_NAME = '#!#@$%'
+
+	ADDRESS = '*********'
+	CITY = '^$^%$&^$'
+	STATE = '4352345'
+	ZIP_CODE = ')(*&^%'
+
+	PHONE = 'dsafasdfas'
+
+	USERNAME = '!@@##$$'
+	PASSWORD = USERNAME + ZIP_CODE
+
+	SSN = 'qwerq wer qwe'
+
+	INIT_BALANCE = 0.00
+	MIN_BALANCE = 0.00
+
