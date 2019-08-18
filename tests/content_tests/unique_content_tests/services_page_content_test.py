6585b0c112ca66c538d5286189f195812e4ff612
Egor Kostan <igorkostan@gmail.com>
2019-08-18 03:57:13 -0700

    Update services_page_content_test.py

--- tests/content_tests/unique_content_tests/services_page_content_test.py
+++ tests/content_tests/unique_content_tests/services_page_content_test.py
@@ -5,7 +5,6 @@
 import allure
 import unittest
 
-from tests.config import Config
 from utils.screenshot import screenshot_on_fail
 from utils.open_web_browser import open_web_browser
 from utils.step_definition import step_definition
@@ -26,12 +25,9 @@ class TestServicesPageContent(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		with allure.step("Open web browser"):
-			# cls.browser = browser_configuration()
-			cls.app_config = Config()
 			cls.page_model = ServicesPageModel
 			cls.page_content = ServicesPageContent
-			cls.page = open_web_browser(browser=cls.app_config.browser,
-			                            page_model=cls.page_model,
+			cls.page = open_web_browser(page_model=cls.page_model,
 			                            page_content=cls.page_content)
 
 	@classmethod
