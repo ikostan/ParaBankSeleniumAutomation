77e32d893808e06121b833ec33722b1caf8c39f3
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:49:40 -0700

    Update user_login_from_about_page_no_such_user_test.py

--- tests/e2e_tests/login_logout/negative/no_such_user/user_login_from_about_page_no_such_user_test.py
+++ tests/e2e_tests/login_logout/negative/no_such_user/user_login_from_about_page_no_such_user_test.py
@@ -2,13 +2,10 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
-#  Created by Egor Kostan.
-#  GitHub: https://github.com/ikostan
-#  LinkedIn: https://www.linkedin.com/in/egor-kostan/
-
 import allure
 import unittest
 
+from tests.config import Config
 from utils.clean_database import clean_database
 from utils.screenshot import screenshot_on_fail
 from utils.open_web_browser import open_web_browser
@@ -35,12 +32,14 @@ class TestUserLoginFromAboutPageNoSuchUserError(unittest.TestCase):
 		cls.user = BaseUser(EmptyFields)
 		cls.page_model = AboutPageModel
 		cls.page_context = AboutPageContent
+		cls.config = Config()
 
 		with allure.step("Initial data setup > clean DB"):
-			clean_database()
+			clean_database(cls.config)
 
 		with allure.step("Open web browser"):
-			cls.page = open_web_browser(page_model=cls.page_model,
+			cls.page = open_web_browser(config=cls.config,
+			                            page_model=cls.page_model,
 			                            page_content=cls.page_context)
 
 	@classmethod
@@ -111,7 +110,7 @@ class TestUserLoginFromAboutPageNoSuchUserError(unittest.TestCase):
 		# URL validation
 		step_definition(self,
 		                step_description='Do URL verification',
-		                expected=LoginPageContent.URL,
+		                expected=self.config.base_url + LoginPageContent.URL,
 		                actual=self.page.url,
 		                act=None,
 		                click=False)
