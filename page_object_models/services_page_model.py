d16a4c15a84d3ed6b2ef9d2d15d0368cf78d27ab
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:46:48 -0700

    Update services_page_model.py

--- page_object_models/services_page_model.py
+++ page_object_models/services_page_model.py
@@ -2,6 +2,9 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
+from tests.config import Config
+from utils.driver import Driver
+
 from page_object_models.base_page_model import BasePageModel
 from expected_results.page_content.services_page_content import ServicesPageContent
 
@@ -12,6 +15,9 @@ class ServicesPageModel(BasePageModel):
 	By following this technique a layer of separation between the test code and technical implementation is created.
 	'''
 
-	_url = ServicesPageContent.URL
+	# _url = ServicesPageContent.URL
 
+	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
+		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+		self._url = config.base_url + ServicesPageContent.URL
 
