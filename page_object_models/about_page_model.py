af22e4db733a66f615020a155e9096508a091d8d
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:45:52 -0700

    Update about_page_model.py

--- page_object_models/about_page_model.py
+++ page_object_models/about_page_model.py
@@ -2,6 +2,9 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
+from tests.config import Config
+from utils.driver import Driver
+
 from element_object_models.element import Element
 from page_object_models.base_page_model import BasePageModel
 from page_locators.about_page_locator import AboutPageLocator
@@ -14,7 +17,11 @@ class AboutPageModel(BasePageModel):
 	By following this technique a layer of separation between the test code and technical implementation is created.
 	'''
 
-	_url = AboutPageContent.URL
+	# _url = AboutPageContent.URL
+
+	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
+		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+		self._url = config.base_url + AboutPageContent.URL
 
 	# @property
 	def description_title(self):
