82f836dc534b4f318c8fd960618545f2f8fd08d8
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:46:30 -0700

    Update login_page_model.py

--- page_object_models/login_page_model.py
+++ page_object_models/login_page_model.py
@@ -6,6 +6,8 @@ from element_object_models.element import Element
 from page_locators.login_page_locator import LoginPageLocator
 from page_object_models.base_page_model import BasePageModel
 from expected_results.page_content.login_page_content import LoginPageContent
+from tests.config import Config
+from utils.driver import Driver
 
 
 class LoginPageModel(BasePageModel):
@@ -14,7 +16,10 @@ class LoginPageModel(BasePageModel):
 	By following this technique a layer of separation between the test code and technical implementation is created.
 	'''
 
-	_url = LoginPageContent.URL
+	# _url = LoginPageContent.URL
+	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
+		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+		self._url = config.base_url + LoginPageContent.URL
 
 	# @property
 	def error_title(self):
