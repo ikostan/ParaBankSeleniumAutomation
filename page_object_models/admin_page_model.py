5d17d8970b4f4827ce186929d934aa6786da4b2d
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:46:00 -0700

    Update admin_page_model.py

--- page_object_models/admin_page_model.py
+++ page_object_models/admin_page_model.py
@@ -3,9 +3,12 @@
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
 from element_object_models.element import Element
+from expected_results.page_content.base_page_content import BasePageContent
 from page_object_models.base_page_model import BasePageModel
 from page_locators.admin_page_locator import AdminPageLocator
 from expected_results.page_content.admin_page_content import AdminPageContent
+from tests.config import Config
+from utils.driver import Driver
 
 
 class AdminPageModel(BasePageModel):
@@ -14,7 +17,9 @@ class AdminPageModel(BasePageModel):
 	By following this technique a layer of separation between the test code and technical implementation is created.
 	'''
 
-	_url = AdminPageContent.URL
+	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
+		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+		self._url = config.base_url + AdminPageContent.URL
 
 	def hit_initialize_button(self):
 		'''
