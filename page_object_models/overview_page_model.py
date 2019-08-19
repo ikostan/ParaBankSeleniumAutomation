fce6ade2849eaabb73c6e5028f78f4dc84b2d26e
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:46:38 -0700

    Update overview_page_model.py

--- page_object_models/overview_page_model.py
+++ page_object_models/overview_page_model.py
@@ -2,6 +2,9 @@
 #  GitHub: https://github.com/ikostan
 #  LinkedIn: https://www.linkedin.com/in/egor-kostan/
 
+from tests.config import Config
+from utils.driver import Driver
+
 from page_object_models.base_page_model import BasePageModel
 from expected_results.page_content.overview_page_content import OverviewPageContent
 
@@ -12,5 +15,8 @@ class OverviewPageModel(BasePageModel):
 	By following this technique a layer of separation between the test code and technical implementation is created.
 	"""
 
-	_url = OverviewPageContent.URL
+	# _url = OverviewPageContent.URL
 
+	def __init__(self, config: Config, driver: Driver, implicit_wait_time, explicit_wait_time):
+		super().__init__(config, driver, implicit_wait_time, explicit_wait_time)
+		_url = config.base_url + OverviewPageContent.URL
