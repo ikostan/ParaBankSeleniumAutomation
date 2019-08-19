211959c5229af38c4dee6e67982a03944ea10fe6
Egor Kostan <igorkostan@gmail.com>
2019-08-19 01:44:44 -0700

    Update clean_database.py

--- utils/clean_database.py
+++ utils/clean_database.py
@@ -5,12 +5,13 @@
 import time
 import allure
 
+from tests.config import Config
 from utils.open_web_browser import open_web_browser
 from page_object_models.admin_page_model import AdminPageModel
 from expected_results.page_content.admin_page_content import AdminPageContent
 
 
-def clean_database():
+def clean_database(config: Config):
 	'''
 	Clean and initialise database.
 	Does not check for any errors.
@@ -18,11 +19,12 @@ def clean_database():
 	'''
 
 	page_model = AdminPageModel
-	page_context = AdminPageContent
+	page_content = AdminPageContent
 
 	print("Open web browser")
-	page = open_web_browser(page_model=page_model,
-	                        page_content=page_context)
+	page = open_web_browser(config=config,
+	                        page_model=page_model,
+	                        page_content=page_content)
 
 	with allure.step("Press 'CLEAN' button"):
 		print("\nCleaning database...")
