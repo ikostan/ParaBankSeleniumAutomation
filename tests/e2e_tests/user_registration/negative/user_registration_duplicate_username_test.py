b74395035f3928ef766700e21b30f7dbd7bbf0ff
Egor Kostan <igorkostan@gmail.com>
2019-08-20 22:26:57 -0700

    Update user_registration_duplicate_username_test.py

--- tests/e2e_tests/user_registration/negative/user_registration_duplicate_username_test.py
+++ tests/e2e_tests/user_registration/negative/user_registration_duplicate_username_test.py
@@ -25,8 +25,6 @@ from expected_results.page_content.register_page_content import RegisterPageCont
 @allure.sub_suite('Negative Tests')
 @allure.feature("Register Page")
 @allure.story('Register Functionality')
-#@pytest.mark.skipif(get_args()['env'] == 'production',
-#                   reason="This is demo test that will have negative effect on Travis CI status")
 @screenshot_on_fail()
 class TestUserRegistrationDuplicateUsername(UserRegistrationCase):
 
